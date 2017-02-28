095439c1cc327bbf98b85c8b1d571ca8c0d7a315
NeZeD [Mac Air] <nezed@ya.ru>
2017-02-28 18:42:03 +0300

    Scoped reset-css

--- components/theme-provider/index.js
+++ components/theme-provider/index.js
@@ -1,8 +1,11 @@
 import React, { PropTypes } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-
+import resetCSS from '!raw-loader!reset-css'
+console.log(resetCSS)
 const GlobalStylesScope = styled.div`
   ${({theme}) => theme.font}
+
+  ${ resetCSS }
 
   *,
   *:before,
