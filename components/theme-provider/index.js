75ebd9a46b23b8a4cd75cf32eefcda4a72fe4613
NeZeD [Mac Air] <nezed@ya.ru>
2017-03-02 17:14:44 +0300

    Get rid of scoped reset-css due its undesirable high selector specificity

--- components/theme-provider/index.js
+++ components/theme-provider/index.js
@@ -1,11 +1,8 @@
 import React, { PropTypes } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
-import resetCSS from '!raw-loader!reset-css'
-console.log(resetCSS)
+
 const GlobalStylesScope = styled.div`
   ${({theme}) => theme.font}
-
-  ${ resetCSS }
 
   *,
   *:before,
--- utils/reset.js
+++ utils/reset.js
@@ -1,5 +1,37 @@
+export const common = `
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+`
+
 export const button = `
   border: none;
   outline: none;
   font: inherit;
+`
+
+// For `ol` and `ul`
+export const list = `
+  list-style: none;
+`
+
+// For `blockquote` and `q`
+export const quote = `
+  quotes: none;
+  &:before, &:after {
+      content: '';
+      content: none;
+  }
+`
+
+export const table = `
+  border-collapse: collapse;
+  border-spacing: 0;
+
+  table, caption, tbody, tfoot, thead, tr, th, td {
+    ${common}
+  }
 `
