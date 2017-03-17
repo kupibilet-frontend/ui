ddeec9c13558f516f8122bf8b1c652303cbe1856
NeZeD [Mac Air] <nezed@ya.ru>
2017-03-17 17:02:12 +0300

    Change file-structure due to fix npm-ignoring

--- components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ThemeProvider } from './theme-provider'
-
-export { default as Button } from './button'
-export { default as DateRange } from './date-range'
-export { default as Icon } from './icons'
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as ThemeProvider } from './components/theme-provider'
+
+export { default as Button } from './components/button'
+export { default as DateRange } from './components/date-range'
+export { default as Icon } from './components/icons'
