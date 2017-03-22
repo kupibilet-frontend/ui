795a25063bfbe18e847c9f95add172f148ea0978
NeZeD [Mac Air] <nezed@ya.ru>
2017-03-22 16:53:43 +0300

    Upgrade Checkbox story with `updateKnob`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as ThemeProvider } from './components/theme-provider'
 
 export { default as Button } from './components/button'
+export { default as Checkbox } from './components/checkbox'
 export { default as DateRange } from './components/date-range'
 export { default as Icon } from './components/icons'
