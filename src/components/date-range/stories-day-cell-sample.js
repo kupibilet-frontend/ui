a04dc04a0b70c8fa723a27b7210c51571ec40b32
NeZeD [Mac Air] <nezed@ya.ru>
2017-03-22 17:37:28 +0300

    Upgrade DateRange story with updateKnob

---
+++ src/components/date-range/stories-day-cell-sample.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const AvgCost = styled.div`
+  font-size: 11px;
+  color: ${({ isCheap, theme }) => (isCheap ? theme.color.success : theme.color.textLight)};
+  letter-spacing: .5px;
+  line-height: 1;
+  text-align: center;
+
+  .CalendarDay:not(.CalendarDay--blocked):hover &,
+  .CalendarDay--selected-start &,
+  .CalendarDay--selected-end & {
+    color: ${({ theme }) => theme.color.background}
+  }
+`
+
+/* eslint-disable react/prop-types */
+const DayCell = ({ day }) => (
+  <span>
+    { day.format('D') }
+    <AvgCost isCheap={(+day.format('DDD') % 9 === 0)}>
+      { Math.floor(day.format('DDD') * 321 % 100) } 000
+    </AvgCost>
+  </span>
+)
+
+export default DayCell
