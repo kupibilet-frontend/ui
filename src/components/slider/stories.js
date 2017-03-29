95f324f142c6b4d28b23871f76f698aed85b402a
Aibeck <mr.aibeck@gmail.com>
2017-03-29 16:33:38 +0300

    slider component

---
+++ src/components/slider/stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { storiesOf } from '@kadira/storybook'
+// import Box from '../box'
+import Slider from './index'
+
+storiesOf('Slider', module)
+  .addWithInfo('Slider', () => (
+    <Slider />
+  ))
