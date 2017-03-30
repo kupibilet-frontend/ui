cd4ae9f2392dcf64de6af8703e43b29efd69dcb7
Aibeck <mr.aibeck@gmail.com>
2017-03-30 09:58:22 +0300

    fixes for pull request

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -1,21 +1,10 @@
-import React, { PropTypes } from 'react'
 import styled from 'styled-components'
 
-export const BoxWrapper = styled.div`
+const Box = styled.div`
   background: ${({ theme }) => (theme.color.background)};
   border: 1px solid ${({ theme }) => (theme.color.miscLighter)};
   border-radius: 6px;
   width: 252px;
 `
-
-function Box(props) {
-  return (
-    <BoxWrapper>{props.children}</BoxWrapper>
-  )
-}
-
-Box.propTypes = {
-  children: PropTypes.node.isRequired,
-}
 
 export default Box
--- src/components/collapse/index.js
+++ src/components/collapse/index.js
@@ -1,12 +1,12 @@
+/* eslint-disable react/require-default-props, react/no-unused-prop-types */
+
 import React, { PropTypes } from 'react'
 import RcCollapse from 'rc-collapse'
 
 import Icon from '../icons'
-import { PanelHeader, IconWrap, PanelContent, RcCollapseWrapper } from './styled'
+import { PanelHeader, PanelContent, RcCollapseWrapper } from './styled'
 
 const RcPanel = RcCollapse.Panel
-
-/* eslint-disable react/require-default-props, react/no-unused-prop-types */
 
 const CollapsePropTypes = {
   children: PropTypes.node.isRequired,
@@ -17,14 +17,14 @@ const CollapsePropTypes = {
 const HeaderComp = (props) => (
   <PanelHeader>
     {props.children}
-    <IconWrap className="icon-wrap">
-      <Icon
-        name="arrow-down"
-        stroke="primaryDarkest"
-        fill="primaryDarkest"
-        size="xxsmall"
-      />
-    </IconWrap>
+    <Icon
+      className="icon"
+      name="arrow-down"
+      stroke="primaryDarkest"
+      fill="primaryDarkest"
+      size="xxsmall"
+      style={{ margin: '3px 0 0 3px', transitionProperty: 'transform' }}
+    />
   </PanelHeader>
 )
 
@@ -50,19 +50,18 @@ const RcPanelWrapper = (props) => {
 
 RcPanelWrapper.propTypes = CollapsePropTypes
 
-export default class Collapse extends React.Component {
-  static Panel = RcPanelWrapper
-
-  static defaultProps = {
-    prefixCls: 'kb-collapse',
-    bordered: true,
-  }
-
-  static propTypes = CollapsePropTypes
-
-  render() {
-    return (
-      <RcCollapseWrapper {...this.props} />
-    )
-  }
+function Collapse(props) {
+  return (
+    <RcCollapseWrapper {...props} />
+  )
 }
+
+export const Panel = RcPanelWrapper
+
+Collapse.defaultProps = {
+  prefixCls: 'kb-collapse',
+}
+
+Collapse.propTypes = CollapsePropTypes
+
+export default Collapse
