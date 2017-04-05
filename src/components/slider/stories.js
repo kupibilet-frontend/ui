748486a054d08f8ce7835aa1298c1da61e8761d6
Aibeck <mr.aibeck@gmail.com>
2017-04-05 19:14:36 +0300

    pit component

--- src/components/slider/stories.js
+++ src/components/slider/stories.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import Slider from './index'
+import { StyledPitComponent } from './styled'
 
-const startDate = new Date(2017, 3, 1).valueOf()
-const endDate = new Date(2017, 3, 2).valueOf()
-const pitPoints = [1491038100000, 1491039000000, 1491039900000, 1491040800000, 1491041700000, 1491048900000, 1491049800000] // eslint-disable-line max-len
+const startDate = new Date(2017, 3, 2, 1).valueOf()
+const endDate = new Date(2017, 3, 2, 2).valueOf()
 
 const snapPoints = [startDate] // startDate чтобы ползунок возвращался обратно
 let i = startDate
@@ -12,16 +12,54 @@ while (i < endDate) {
   snapPoints.push(i += 900000)
 }
 
+const pitPoints = snapPoints.slice(0, snapPoints.length - 1)
+
+const ticketsCount = {
+  1491084000000: 3,
+  1491084900000: 10,
+  1491085800000: 0,
+  1491086700000: 2,
+}
+
+
+class LabeledSlider extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      values: props.values || [0],
+    }
+
+    this.updateValue = this.updateValue.bind(this)
+  }
+
+  updateValue(sliderState) {
+    this.setState({
+      values: sliderState.values,
+    })
+  }
+
+  render() {
+    return (
+      <div style={{ width: '252px' }}>
+        <Slider
+          {...this.props}
+          onValuesUpdated={this.updateValue}
+          min={startDate}
+          max={endDate}
+          snap
+          snapPoints={snapPoints}
+          pitPoints={pitPoints}
+          values={this.state.values}
+          pitComponent={() => <StyledPitComponent ticketsCount={ticketsCount} />}
+        />
+      </div>
+    )
+  }
+}
+
+
 storiesOf('Slider', module)
   .addWithInfo('Slider', () => (
-    <div style={{ width: '252px' }}>
-      <Slider
-        min={startDate}
-        max={endDate}
-        snap
-        snapPoints={snapPoints}
-        pitPoints={pitPoints}
-        values={[startDate, endDate]}
-      />
-    </div>
+    <LabeledSlider values={[startDate, endDate]} />
   ))
