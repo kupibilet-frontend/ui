a7410977434396ce7949aabe2d284cd0e493f5f4
Aibeck <mr.aibeck@gmail.com>
2017-04-05 14:01:49 +0300

    slider component

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,93 +1,38 @@
 import React, { PropTypes } from 'react'
-import Rheostat from 'rheostat'
-import styled from 'styled-components'
+import { RheostatWrapper, StyledHandle, StyledProgressBar, StyledPitComponent } from './styled'
 
-export const RheostatWrapper = styled(Rheostat)`
-  width: 252px;
-
-  .rheostat-background {
-    background-color: ${({ theme }) => theme.color.miscLight};
-    height: 6px;
-    border-radius: 3px;
-    width: 100%;
-  }
-`
-
-export const StyledHandle = styled.span`
-  background-color: ${({ theme }) => theme.color.miscDark};
-  border-radius: 2rem;
-  cursor: pointer;
-  display: inline-block;
-  height: 18px;
-  top: -6px;
-  width: 18px;
-  z-index: 1;
-
-  &:hover, &:active {
-    background-color: ${({ theme }) => theme.color.primary};
-  }
-`
-
-export const StyledProgressBar = styled.span`
-  background-color: ${({ theme }) => theme.color.primaryLighter};
-  border-radius: 3px;
-  display: flex;
-  position: absolute;
-  height: 6px;
-  top: 0;
-  z-index: 0;
-`
-
-export const StyledPitComponent = styled.div`
-  background: ${({ theme }) => theme.color.miscLighter};
-  width: 12px;
-  height: ${(props) => (props.children % 10 === 0 ? 12 : 8)}px;
-  top: 20px;
-`
-
-function PitComponent({ style, children }) {
+function Slider(props) {
   return (
-    <div
-      style={{
-        ...style,
-        background: '#dde3eb',
-        width: 12,
-        height: children % 10 === 0 ? 12 : 8,
-        top: `-${children % 10 === 0 ? 12 : 8}`,
-      }}
+    <RheostatWrapper
+      handle={StyledHandle}
+      progressBar={StyledProgressBar}
+      pitComponent={StyledPitComponent}
+      min={props.min}
+      max={props.max}
+      pitPoints={props.pitPoints}
+      snap={props.snap}
+      snapPoints={props.snapPoints}
+      values={props.values}
     />
   )
 }
 
-PitComponent.propTypes = {
-  style: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-  children: PropTypes.number,
+Slider.defaultProps = {
+  min: 0,
+  max: 100,
+  pitPoints: [],
+  snap: false,
+  snapPoints: [],
+  values: [0, 100],
 }
 
-PitComponent.defaultProps = {
-  style: null,
-  children: null,
+Slider.propTypes = {
+  min: PropTypes.number,
+  max: PropTypes.number,
+  pitPoints: PropTypes.arrayOf(PropTypes.number),
+  snap: PropTypes.bool,
+  snapPoints: PropTypes.arrayOf(PropTypes.number),
+  values: PropTypes.arrayOf(PropTypes.number),
 }
 
-export default class Slider extends React.Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  }
-
-  static defaultProps = {
-  }
-
-  render() {
-    return (
-      <RheostatWrapper
-        min={0}
-        max={100}
-        handle={StyledHandle}
-        progressBar={StyledProgressBar}
-        pitComponent={PitComponent}
-        pitPoints={[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]} // eslint-disable-line max-len
-        values={[0, 100]}
-      />
-    )
-  }
-}
+export default Slider
--- src/components/slider/styled.js
+++ src/components/slider/styled.js
@@ -1,5 +1,59 @@
+import Rheostat from 'rheostat'
 import styled from 'styled-components'
 
-export const CheckboxInput = styled.input`
-  display: none;
+import { switchTransition } from '../../utils/transitions'
+
+const getHandleColor = (props) => {
+  if (props['data-handle-key'] === 0 && props['aria-valuenow'] !== props['aria-valuemin']) {
+    return props.theme.color.primary
+  } else if (props['data-handle-key'] === 1 && props['aria-valuenow'] !== props['aria-valuemax']) {
+    return props.theme.color.primary
+  } return props.theme.color.miscDark
+}
+
+export const RheostatWrapper = styled(Rheostat)`
+  width: 252px;
+
+  .rheostat-background {
+    background-color: ${({ theme }) => theme.color.miscLight};
+    height: 6px;
+    border-radius: 3px;
+    width: 100%;
+  }
+`
+
+export const StyledHandle = styled.span`
+  ${switchTransition}
+  transition-property: background-color, box-shadow;
+  background-color: ${(props) => getHandleColor(props)}
+  border-radius: 2rem;
+  cursor: pointer;
+  display: inline-block;
+  height: 18px;
+  top: -6px;
+  width: 18px;
+  z-index: 1;
+
+  &:hover, &:active {
+    background-color: ${({ theme }) => theme.color.primary};
+    box-shadow: 0 0 0 1px ${({ theme }) => theme.color.primary};
+  }
+`
+
+export const StyledProgressBar = styled.span`
+  background-color: ${({ theme }) => theme.color.primaryLighter};
+  display: flex;
+  position: absolute;
+  height: 6px;
+  top: 0;
+  z-index: 0;
+`
+
+export const StyledPitComponent = styled.span`
+  background: ${({ theme }) => theme.color.miscLighter};
+  width: 12px;
+  height: ${(props) => (props.children % 450000 === 0 ? 12 : 8)}px;
+  overflow: hidden;
+  text-indent: 100%;
+  top: -${(props) => (props.children % 10 === 0 ? 12 : 8)}px;
 `
