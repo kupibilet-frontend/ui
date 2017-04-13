a5baf0961a791108ac9c81f8dc40763f0f64c916
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-13 20:16:10 +0300

    Rename Box story

--- src/components/box/stories.js
+++ src/components/box/stories.js
@@ -3,7 +3,7 @@ import { storiesOf } from '@kadira/storybook'
 import Box from '../box'
 
 storiesOf('Box', module)
-  .addWithInfo('Box', () => (
+  .addWithInfo('Defalut', () => (
     <Box>
       <p>box content</p>
     </Box>
