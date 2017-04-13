5c5f7f061d8259180d76a10d14a9458630193418
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-13 20:17:26 +0300

    Create FilterSection

--- src/index.js
+++ src/index.js
@@ -5,3 +5,5 @@ export { default as Checkbox } from './components/checkbox'
 export { default as DateRange } from './components/date-range'
 export { default as Icon } from './components/icons'
 export { default as Box } from './components/box'
+
+export { default as FilterSection } from './blocks/FilterSection'
