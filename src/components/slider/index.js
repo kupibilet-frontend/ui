b7203d78d8ce346252042b382f10938efed2cf91
Aibeck <mr.aibeck@gmail.com>
2017-04-14 18:27:12 +0300

    slider component

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -1,25 +1,130 @@
-import { PropTypes } from 'react'
-import { Slider, StyledHandle, StyledProgressBar, StyledPitComponent } from './styled'
+import React, { PropTypes } from 'react'
+import { StyledSlider, StyledHandle, StyledProgressBar, StyledPitComponent } from './styled'
+
+
+class Slider extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      values: props.values || [0],
+      pitPoints: this.getPitPoints(props.sliderData),
+      snapPoints: this.getSnapPoints(props),
+      pitHeight: this.getPitHeight(props.sliderData),
+      pitWidth: this.getPitWidth(props.sliderData),
+    }
+
+    this.updateValue = this.updateValue.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.sliderData !== nextProps.sliderData) {
+      this.setState({
+        pitPoints: this.getPitPoints(nextProps.sliderData),
+      })
+      this.setState({
+        pitHeight: this.getPitHeight(nextProps.sliderData),
+      })
+      this.setState({
+        pitWidth: this.getPitWidth(nextProps.sliderData),
+      })
+    }
+
+    if (this.props.step !== nextProps.step) {
+      this.setState({
+        snapPoints: this.getSnapPoints(nextProps),
+      })
+    }
+  }
+
+  getPitPoints = (sliderData) => (
+    Object.keys(sliderData)
+  )
+
+  getSnapPoints = (props) => {
+    const snapPointsArray = [props.values[0]] // чтобы ползунок возвращался на первую позицию
+    let i = props.min
+    while (i < props.max) {
+      snapPointsArray.push(i += props.step)
+    }
+    return snapPointsArray
+  }
+
+  getPitHeight = (sliderData) => {
+    const maxHeight = Math.max(...Object.values(sliderData))
+    const onePercent = maxHeight / 12
+
+    const pitHeight = Object.keys(sliderData).reduce((result, item) => {
+      const updatedResult = { ...result }
+      updatedResult[item] = sliderData[item] / onePercent
+      return updatedResult
+    }, {})
+    return pitHeight
+  }
+
+  getPitWidth = (sliderData) => {
+    const width = 100 / Object.keys(sliderData).length
+    return width
+  }
+
+  updateValue(sliderState) {
+    this.setState({
+      values: sliderState.values,
+    })
+  }
+
+  render() {
+    const {
+      min,
+      max,
+      snap,
+      step,
+    } = this.props
+
+    return (
+      <StyledSlider
+        {...this.props}
+        onValuesUpdated={this.updateValue}
+        min={min}
+        max={max}
+        snap={snap}
+        snapPoints={this.state.snapPoints}
+        pitPoints={this.state.pitPoints}
+        values={this.state.values}
+        step={step}
+        pitComponent={(props) =>
+          <StyledPitComponent
+            {...props}
+            pitWidth={this.state.pitWidth}
+            pitHeight={this.state.pitHeight}
+          />
+        }
+      />
+    )
+  }
+}
 
 Slider.defaultProps = {
   min: 0,
   max: 100,
   pitPoints: [],
-  snap: false,
+  snap: true,
   snapPoints: [],
   values: [0, 100],
   handle: StyledHandle,
   progressBar: StyledProgressBar,
   pitComponent: StyledPitComponent,
+  sliderData: {},
+  step: 0,
 }
 
 Slider.propTypes = {
   min: PropTypes.number,
   max: PropTypes.number,
-  pitPoints: PropTypes.arrayOf(PropTypes.number),
   snap: PropTypes.bool,
-  snapPoints: PropTypes.arrayOf(PropTypes.number),
   values: PropTypes.arrayOf(PropTypes.number),
+  sliderData: PropTypes.object,
+  step: PropTypes.number,
 }
 
 export default Slider
--- src/components/slider/stories.js
+++ src/components/slider/stories.js
@@ -1,18 +1,9 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import Slider from './index'
-import { StyledPitComponent } from './styled'
 
 const startDate = new Date(2017, 3, 2, 1).valueOf()
 const endDate = new Date(2017, 3, 2, 2).valueOf()
-
-const snapPoints = [startDate] // startDate чтобы ползунок возвращался обратно
-let i = startDate
-while (i < endDate) {
-  snapPoints.push(i += 900000)
-}
-
-const pitPoints = snapPoints.slice(0, snapPoints.length - 1)
 
 const ticketsCount = {
   1491084000000: 3,
@@ -21,70 +12,15 @@ const ticketsCount = {
   1491086700000: 2,
 }
 
-const objValues = Object.values(ticketsCount)
-const maxHeight = Math.max(...objValues)
-const width = 100 / objValues.length
-const onePercent = maxHeight / 12
-
-const pitHeight = Object.keys(ticketsCount).reduce((result, item, key) => {
-  const updatedResult = { ...result }
-  updatedResult[item] = objValues[key] / onePercent
-  return updatedResult
-}, {})
-
-
-class LabeledSlider extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      values: props.values || [0],
-    }
-
-    this.updateValue = this.updateValue.bind(this)
-  }
-
-  updateValue(sliderState) {
-    this.setState({
-      values: sliderState.values,
-    })
-  }
-
-  render() {
-    return (
-      <div style={{ width: '252px' }}>
-        <Slider
-          {...this.props}
-          onValuesUpdated={this.updateValue}
-          min={startDate}
-          max={endDate}
-          snap
-          snapPoints={snapPoints}
-          pitPoints={pitPoints}
-          values={this.state.values}
-          pitComponent={(props) =>
-            <StyledPitComponent
-              {...props}
-              pitWidth={width}
-              pitHeight={pitHeight}
-            />
-          }
-        />
-      </div>
-    )
-  }
-}
-
-LabeledSlider.defaultProps = {
-  values: [],
-}
-
-LabeledSlider.propTypes = {
-  values: PropTypes.array,
-}
-
-
 storiesOf('Slider', module)
   .addWithInfo('Slider', () => (
-    <LabeledSlider values={[startDate, endDate]} />
+    <div style={{ width: '252px' }}>
+      <Slider
+        sliderData={ticketsCount}
+        min={startDate}
+        max={endDate}
+        values={[startDate, endDate]}
+        step={900000}
+      />
+    </div>
   ))
