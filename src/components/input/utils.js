0f5343ed492c14617b9d9c4340c4bea87ba904ca
s-kobets <s-kobets617@yandex.ru>
2017-04-14 17:20:46 +0300

    add formula for calculate height input

--- src/components/input/utils.js
+++ src/components/input/utils.js
@@ -4,14 +4,21 @@ export const PADDING = {
   small: 9,
 }
 
-export const HEIGHT = {
-  large: 42,
-  normal: 36,
-  small: 30,
-}
-
 export const TYPOGRAPHY = {
   large: 18,
   normal: 16,
   small: 16,
+}
+
+const arr = []
+let element
+// eslint-disable-next-line
+for (element in PADDING) {
+  arr.push(PADDING[element])
+}
+
+const customNumber = arr.reduce((x, y) => Math.abs(x - y))
+
+export function calculate(size) {
+  return (PADDING[size] * 2 + customNumber * 2)
 }
