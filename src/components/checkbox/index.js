f35bc4130002e88c8223814794ccbd396c8d4792
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-17 16:22:22 +0300

    Migrate to prop-types

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -1,4 +1,5 @@
-import React, { PropTypes, PureComponent } from 'react'
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 import Icon from '../icons'
 import { CheckboxLabel, StyledCheckbox, IconWrap, CheckboxInput, LabelText } from './styled'
 
