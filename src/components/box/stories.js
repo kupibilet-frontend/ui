cff3ff6a5a1b0bd0cf54dd68f95b8e2603d86e02
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-17 14:49:57 +0300

    Fix import

--- src/components/box/stories.js
+++ src/components/box/stories.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
-import Box from '../box'
+import Box from './index'
 
 storiesOf('Box', module)
   .addWithInfo('Defalut', () => (
