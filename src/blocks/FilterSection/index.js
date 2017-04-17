7b68b4d11ce541d999139ee29260de027191c798
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-17 16:22:22 +0300

    Migrate to prop-types

--- src/blocks/FilterSection/index.js
+++ src/blocks/FilterSection/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable consistent-return */
 
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 import { styles as boxStyles } from '../../components/box'
 import Collapse from '../../components/collapse'
