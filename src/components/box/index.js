13c2956d3f83d1b9e3c728c0bbeedc585c8b0cae
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-17 14:50:56 +0300

    Export box styles

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -1,9 +1,13 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-const Box = styled.div`
+export const styles = css`
   background: ${({ theme }) => (theme.color.background)};
   border: 1px solid ${({ theme }) => (theme.color.miscLighter)};
   border-radius: 6px;
+`
+
+const Box = styled.div`
+  ${styles}
 `
 
 export default Box
