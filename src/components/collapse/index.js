4c76ef33923c3623a7e41f72fc60c1468951c8e5
Valentin Semirulnik <v7rulnik@gmail.com>
2017-04-18 13:21:05 +0300

    Remove prefix overwriting

--- src/components/collapse/index.js
+++ src/components/collapse/index.js
@@ -2,9 +2,5 @@ import RcCollapse from 'rc-collapse'
 import Panel from './Panel'
 
 RcCollapse.Panel = Panel
-RcCollapse.defaultProps = {
-  ...RcCollapse.defaultProps,
-  prefixCls: 'kb-collapse',
-}
 
 export default RcCollapse
