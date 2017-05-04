1f379ce06139fa98fd041d16906998a115195688
skobets <sergey.kobets@kupibilet.ru>
2017-05-04 18:45:38 +0300

    add component passenger

---
+++ src/components/passenger/index.js
@@ -0,0 +1,114 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { color } from '../theme-provider/theme'
+
+const Passenger = styled.div`
+  display: flex;
+  width: 100%;
+  min-width: 216px;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+`
+
+const Title = styled.p`
+  margin: 0;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${color.textDarker};
+`
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 11px;
+  line-height: 16px;
+  color: ${color.text};
+`
+
+const Current = styled.span`
+  margin-right: 6px;
+  font-size: 24px;
+  line-height: 28px;
+  color: ${color.textDarker};
+`
+
+const Spinner = styled.div`
+  position: relative;
+  display: inline-flex;
+  margin-left: 6px;
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  background-color: ${color.primary};
+  opacity: ${(props) => (props.disabled === true ? 0.2 : 1)};
+  cursor: ${(props) => (props.disabled === true ? 'default' : 'pointer')};
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: calc(50% - 1px);
+    left: calc(50% - 6px);
+    display: inline-block;
+    height: 2px;
+    width: 12px;
+    border-radius: 2px;
+    background-color: #fff;
+  }
+
+  &.plus {
+    &:after {
+      content: '';
+      position: absolute;
+      top: calc(50% - 1px);
+      left: calc(50% - 6px);
+      display: inline-block;
+      height: 2px;
+      width: 12px;
+      border-radius: 2px;
+      background-color: #fff;
+      transform: rotate(90deg);
+    }
+  }
+`
+
+const PassengerCurrent = styled.div`
+  display: flex;
+`
+
+const PassengerSpinner = styled.div`
+  display: inline-flex;
+  align-items: center;
+`
+
+const PassengerComponent = ({ title, description, current, disabled }) => (
+  <Passenger>
+    <div>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </div>
+    <PassengerCurrent>
+      <Current>{current}</Current>
+      <PassengerSpinner>
+        <Spinner disabled={disabled} />
+        <Spinner className="plus" disabled={disabled} />
+      </PassengerSpinner>
+    </PassengerCurrent>
+  </Passenger>
+)
+
+PassengerComponent.defaultProps = {
+  title: '',
+  description: '',
+  current: 0,
+  disabled: false,
+}
+
+PassengerComponent.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  current: PropTypes.number,
+  disabled: PropTypes.bool,
+}
+
+export default PassengerComponent
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ export { default as CalendarDay } from './components/calendar-day'
 export { default as Icon } from './components/icons'
 export { default as Collapse } from './components/collapse'
 export { default as Box } from './components/box'
+export { default as Passenger } from './components/passenger'
 
 export { default as FilterSection } from './blocks/FilterSection'
 
