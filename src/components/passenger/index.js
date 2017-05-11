e64c66369e95c0568306da363be24e8d42ac8426
skobets <sergey.kobets@kupibilet.ru>
2017-05-11 19:32:06 +0300

    fix style for button control

--- src/components/passenger/index.js
+++ src/components/passenger/index.js
@@ -1,118 +1,61 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { color } from '../theme-provider/theme'
+import {Passenger, Title, Description, Current, CountControl, PassangerCount, CountControls} from './styled'
+import Icon from '../icons'
+import Button from '../button/index'
 
-const Passenger = styled.div`
-  display: flex;
-  width: 100%;
-  min-width: 216px;
-  align-items: center;
-  justify-content: space-between;
-  flex-wrap: nowrap;
-`
-
-const Title = styled.p`
-  margin: 0;
-  font-size: 16px;
-  line-height: 20px;
-  color: ${color.textDarker};
-`
-
-const Description = styled.p`
-  margin: 0;
-  font-size: 11px;
-  line-height: 16px;
-  color: ${color.text};
-`
-
-const Current = styled.span`
-  margin-right: 6px;
-  font-size: 24px;
-  line-height: 28px;
-  color: ${color.textDarker};
-`
-
-const Spinner = styled.div`
-  position: relative;
-  display: inline-flex;
-  margin-left: 6px;
-  height: 30px;
-  width: 30px;
-  border-radius: 50%;
-  background-color: ${color.primary};
-  opacity: ${(props) => (props.disabled === true ? 0.2 : 1)};
-  cursor: ${(props) => (props.disabled === true ? 'default' : 'pointer')};
-
-  &:before {
-    content: '';
-    position: absolute;
-    top: calc(50% - 1px);
-    left: calc(50% - 6px);
-    display: inline-block;
-    height: 2px;
-    width: 12px;
-    border-radius: 2px;
-    background-color: #fff;
-  }
-
-  &.plus {
-    &:after {
-      content: '';
-      position: absolute;
-      top: calc(50% - 1px);
-      left: calc(50% - 6px);
-      display: inline-block;
-      height: 2px;
-      width: 12px;
-      border-radius: 2px;
-      background-color: #fff;
-      transform: rotate(90deg);
-    }
-  }
-`
-
-const PassengerCurrent = styled.div`
-  display: flex;
-`
-
-const PassengerSpinner = styled.div`
-  display: inline-flex;
-  align-items: center;
-`
-
-const PassengerComponent = ({ title, description, current, disabled, decrement, increment }) => (
-  <Passenger>
+const PassangerPicker = ({ title, description, value, decrement, increment, ...props }) => (
+  <Passenger
+    {...props}
+  >
     <div>
       <Title>{title}</Title>
       <Description>{description}</Description>
     </div>
-    <PassengerCurrent>
-      <Current>{current}</Current>
-      <PassengerSpinner>
-        <Spinner disabled={(disabled === 'disabled-min' || disabled === 'disabled') && true} onClick={decrement} />
-        <Spinner className="plus" disabled={(disabled === 'disabled-max' || disabled === 'disabled') && true} onClick={increment} />
-      </PassengerSpinner>
-    </PassengerCurrent>
+    <PassangerCount>
+      <Current>{value}</Current>
+      <CountControls>
+        <CountControl className="control-min" isMax={props.isMax} isMin={props.isMin} onClick={decrement}>
+          <Button size="normal" icon={
+            <Icon 
+              name="minus"
+              stroke="background"
+            />
+          } />
+        </CountControl>
+        <CountControl className="control-max" isMax={props.isMax} isMin={props.isMin} onClick={increment}>
+          <Button size="normal" icon={
+            <Icon 
+              name="plus"
+              stroke="background"
+            />
+          } />
+        </CountControl>
+      </CountControls>
+    </PassangerCount>
   </Passenger>
 )
 
-PassengerComponent.defaultProps = {
+PassangerPicker.defaultProps = {
   title: '',
   description: '',
   current: 0,
   disabled: '',
   decrement: null,
   increment: null,
+  isMax: false,
+  isMin: true,
 }
 
-PassengerComponent.propTypes = {
+PassangerPicker.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
   current: PropTypes.number,
   disabled: PropTypes.string,
   decrement: PropTypes.func,
   increment: PropTypes.func,
+  isMin: PropTypes.bool,
+  isMax: PropTypes.bool,
 }
 
-export default PassengerComponent
+export default PassangerPicker
--- src/components/passenger/stories.js
+++ src/components/passenger/stories.js
@@ -1,34 +1,38 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { text, number, boolean } from '@kadira/storybook-addon-knobs'
-import Passenger from './index'
+import PassangerPicker from './index'
+import updateKnob from '../../utils/updateKnob'
 
 const defaultDescription = {
   title: 'Взрослые',
   description: 'До 2 лет, без места',
   current: 1,
-  disabled: 'disabled-min',
+  isMax: false,
+  isMin: true,
 }
 
-storiesOf('Passenger', module)
+storiesOf('PassangerPicker', module)
   .addWithInfo('Defalut', () => {
     const title = text('title', defaultDescription.title)
     const description = text('description', defaultDescription.description)
-    let current = number('current', defaultDescription.current)
-    const disabled = text('disabled', defaultDescription.disabled)
+    let value = number('value', defaultDescription.current)
+    const isMax = boolean('isMax', defaultDescription.isMax)
+    const isMin = boolean('isMin', defaultDescription.isMin)
     const decrement = () => {
-      current -= 1
+      updateKnob('value', 'number', value -= 1)
     }
     const increment = () => {
-      current += 1
+      updateKnob('value', 'number', value += 1)
     }
 
     return (
-      <Passenger
+      <PassangerPicker
         title={title}
         description={description}
-        current={current}
-        disabled={disabled}
+        value={value}
+        isMax={isMax}
+        isMin={isMin}
         decrement={decrement}
         increment={increment}
       />
