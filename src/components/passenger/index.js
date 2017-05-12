008d4e76f1fcdaad7d60eec619a52d29b9054626
skobets <sergey.kobets@kupibilet.ru>
2017-05-12 18:13:18 +0300

    end style for button counter

--- src/components/passenger/index.js
+++ src/components/passenger/index.js
@@ -1,40 +1,104 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Passenger, Title, Description, Current, CountControl, PassangerCount, CountControls} from './styled'
 import Icon from '../icons'
+import styled from 'styled-components'
+import { button } from '../../utils/reset'
 import Button from '../button/index'
 
-const PassangerPicker = ({ title, description, value, decrement, increment, ...props }) => (
-  <Passenger
-    {...props}
-  >
-    <div>
-      <Title>{title}</Title>
-      <Description>{description}</Description>
-    </div>
-    <PassangerCount>
-      <Current>{value}</Current>
-      <CountControls>
-        <CountControl className="control-min" isMax={props.isMax} isMin={props.isMin} onClick={decrement}>
-          <Button size="normal" icon={
-            <Icon 
-              name="minus"
-              stroke="background"
-            />
-          } />
-        </CountControl>
-        <CountControl className="control-max" isMax={props.isMax} isMin={props.isMin} onClick={increment}>
-          <Button size="normal" icon={
-            <Icon 
-              name="plus"
-              stroke="background"
-            />
-          } />
-        </CountControl>
-      </CountControls>
-    </PassangerCount>
-  </Passenger>
-)
+const Passenger = styled.div`
+  display: flex;
+  width: 100%;
+  min-width: 216px;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+`
+
+const Title = styled.p`
+  margin: 0;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.color.textDarker};
+`
+
+const Description = styled.p`
+  margin: 0;
+  font-size: 11px;
+  line-height: 16px;
+  color: ${({ theme }) => theme.color.text};
+`
+
+const Current = styled.span`
+  margin-right: 6px;
+  font-size: 24px;
+  line-height: 28px;
+  color: ${({ theme }) => theme.color.textDarker};
+`
+
+const ButtonControl = styled(Button)`
+  margin-left: 6px;
+`
+
+const PassangerCount = styled.div`
+  display: flex;
+`
+
+const CountControls = styled.div`
+  display: inline-flex;
+  align-items: center;
+`
+
+class PassangerPicker extends React.PureComponent {
+  decrement = () => {
+    if(!this.props.isMin) {
+      this.props.onChange(this.props.value - 1)
+    }
+  }
+
+  increment = () => {
+    if(!this.props.isMax) {
+      this.props.onChange(this.props.value + 1)
+    }
+  }
+
+  render() {
+    const { title, description, value, onChange, isMin, isMax } = this.props
+  
+    return (
+      <Passenger>
+        <div>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
+        </div>
+        <PassangerCount>
+          <Current>{value}</Current>
+          <CountControls>
+            <ButtonControl 
+              size="normal"
+              disabled={isMin}
+              onClick={this.decrement}
+              icon={
+                <Icon 
+                  name="minus"
+                  stroke="background"
+                />
+            } />
+            <ButtonControl
+              size="normal"
+              disabled={isMax}
+              onClick={this.increment}
+              icon={
+                <Icon 
+                  name="plus"
+                  stroke="background"
+                />
+            } />
+          </CountControls>
+        </PassangerCount>
+      </Passenger>
+    )
+  }
+}
 
 PassangerPicker.defaultProps = {
   title: '',
