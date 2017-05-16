e421df672f7490580b0ba2193e41d872428dbc8f
NeZeD [Mac Air] <nezed@ya.ru>
2017-05-16 19:39:21 +0300

    Button: fix large size and add ControlsGroup support

--- src/components/controls-group/stories.js
+++ src/components/controls-group/stories.js
@@ -5,15 +5,27 @@ import { text } from '@kadira/storybook-addon-knobs'
 import updateKnob from '../../utils/updateKnob'
 
 import AirportInput from '../airport-input'
+import Button from '../button'
 import ControlsGroup from './index'
 
 storiesOf('ControlsGroup', module)
-  .addWithInfo('Small Icon with stroke', () => (
+  .addWithInfo('Inputs group', () => (
     <ControlsGroup onSwap={action('Swap it!')}>
       <AirportInput readOnly value="Шереметьево" city="Москва" IATACode="SVO" />
       <AirportInput
         value={text('value', 'Несуществующий город')}
         onChange={({ target }) => updateKnob('value', 'text', target.value)}
       />
+    </ControlsGroup>
+  ))
+  .addWithInfo('Different controls group', () => (
+    <ControlsGroup>
+      <Button size="large">
+        Пойти
+      </Button>
+      <AirportInput readOnly value="Шереметьево" city="Москва" IATACode="SVO" />
+      <Button size="large">
+        Найти
+      </Button>
     </ControlsGroup>
   ))
