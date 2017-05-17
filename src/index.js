9e13cf70ed2c4250e7f2d48ad62866dc9c003a90
NeZeD [Mac Air] <nezed@ya.ru>
2017-05-17 16:19:58 +0300

    Describe kinds of UI-components in index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 // @flow
 export { default as ThemeProvider } from './components/theme-provider'
 
+// Base controls and elemtnts
 export { default as Button } from './components/button'
 export { default as Checkbox } from './components/checkbox'
+export { default as Icon } from './components/icons'
+
+// Base layout particles
+export { default as FilterSection } from './blocks/FilterSection'
+export { default as Box } from './components/box'
+export { default as ControlsGroup } from './components/controls-group'
+export { FluidContainer, FluidSection, Aside, SingleFluidSection } from './components/fluid-layout'
+
+// Specific behaviour non-visual components
+export { default as Collapse } from './components/collapse'
+
+// Specific controls
 export { default as DateRange } from './components/date-range'
 export { default as CalendarDay } from './components/calendar-day'
-export { default as Icon } from './components/icons'
-export { default as Collapse } from './components/collapse'
-export { default as Box } from './components/box'
-export { default as PassengerPicker } from './components/passenger'
-
-export { default as FilterSection } from './blocks/FilterSection'
 
 export { default as AirportInput } from './components/airport-input'
 export { default as AirportSuggest } from './components/airport-suggest'
 export { default as Autocomplete } from './components/autocomplete'
-export { default as ControlsGroup } from './components/controls-group'
+
+export { default as PassengerPicker } from './components/passenger'
