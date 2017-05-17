a5b86a00b31ac7e4721a89b98588a7683bafcefd
skobets <sergey.kobets@kupibilet.ru>
2017-05-17 10:54:25 +0300

    add props isClick for button and rename component

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -3,11 +3,12 @@ import PropTypes from 'prop-types'
 import { findDOMNode } from 'react-dom'
 import styled from 'styled-components'
 
-const RcDropdownWrapper = styled.div`
+const DropdownWrapper = styled.div`
   position: relative;
+  display: inline-flex;
 `
 
-const Dropdown = styled.div`
+const DropdownOverlay = styled.div`
   position: absolute;
   top: calc(100% + 3px);
   left: 0;
@@ -16,7 +17,7 @@ const Dropdown = styled.div`
   box-shadow: 0 2px 4px 0 rgba(98,112,139,0.6);
 `
 
-class RcDropdown extends React.PureComponent {
+class Dropdown extends React.PureComponent {
   constructor(props) {
     super(props)
     this.state = {
@@ -45,34 +46,43 @@ class RcDropdown extends React.PureComponent {
   render() {
     const { button, template } = this.props
     const { isOpen } = this.state
+    const dropdownButtonActive = React.cloneElement(button,
+      {
+        isClick: true,
+        onClick: () => { this.setState({ isOpen: !isOpen }) },
+      },
+    )
     const dropdownButton = React.cloneElement(button,
-      { onClick: () => { this.setState({ isOpen: !isOpen }) } },
+      {
+        isClick: false,
+        onClick: () => { this.setState({ isOpen: !isOpen }) },
+      },
     )
 
     return (
-      <RcDropdownWrapper
+      <DropdownWrapper
         ref={(name) => { this.area = name }}
       >
-        {dropdownButton}
+        { isOpen ? dropdownButtonActive : dropdownButton }
         {
           isOpen &&
-          <Dropdown>
+          <DropdownOverlay>
             {template}
-          </Dropdown>
+          </DropdownOverlay>
         }
-      </RcDropdownWrapper>
+      </DropdownWrapper>
     )
   }
 }
 
-RcDropdown.defaultProps = {
+Dropdown.defaultProps = {
   button: <button>Toggle</button>,
   template: <div>Hello World</div>,
 }
 
-RcDropdown.propTypes = {
+Dropdown.propTypes = {
   button: PropTypes.object.isRequired,
   template: PropTypes.object.isRequired,
 }
 
-export default RcDropdown
+export default Dropdown
