ffee6c1601e7ba13ff81eb3d01caf67d5828d472
Aibeck <mr.aibeck@gmail.com>
2017-06-09 15:41:34 +0300

    add handheld breakpoints

--- src/utils/media-queries.js
+++ src/utils/media-queries.js
@@ -2,17 +2,22 @@ import { css } from 'styled-components'
 
 const media = {
   mobile: (...args) => css`
-    @media (max-width: 639px) {
+    @media screen and (max-width: 639px) {
       ${css(...args)}
     }
   `,
   tablet: (...args) => css`
-    @media (max-width: 768px) {
+    @media screen and (min-width: 640px) and (max-width: 768px) {
+      ${css(...args)}
+    }
+  `,
+  handheld: (...args) => css`
+    @media screen and (max-width: 768px) {
       ${css(...args)}
     }
   `,
   desktop: (...args) => css`
-    @media (min-width: 769px) {
+    @media screen and (min-width: 769px) {
       ${css(...args)}
     }
   `,
