5cc5dbf0dfecc2dbabc627b9518525f490c1556c
Aibeck <mr.aibeck@gmail.com>
2017-06-13 11:36:20 +0300

    change breakpoints for media queries

--- src/utils/media-queries.js
+++ src/utils/media-queries.js
@@ -2,22 +2,22 @@ import { css } from 'styled-components'
 
 const media = {
   mobile: (...args) => css`
-    @media screen and (max-width: 639px) {
+    @media screen and (max-width: 599px) {
       ${css(...args)}
     }
   `,
   tablet: (...args) => css`
-    @media screen and (min-width: 640px) and (max-width: 768px) {
+    @media screen and (min-width: 600px) and (max-width: 999px) {
       ${css(...args)}
     }
   `,
   handheld: (...args) => css`
-    @media screen and (max-width: 768px) {
+    @media screen and (max-width: 999px) {
       ${css(...args)}
     }
   `,
   desktop: (...args) => css`
-    @media screen and (min-width: 769px) {
+    @media screen and (min-width: 1000px) {
       ${css(...args)}
     }
   `,
