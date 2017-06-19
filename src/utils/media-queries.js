27a2288dcd6863ecf9df687f318b31c3e0b19bba
NeZeD [Mac Air] <nezed@ya.ru>
2017-06-19 17:25:23 +0300

    Add Media HOC

--- src/utils/media-queries.js
+++ src/utils/media-queries.js
@@ -1,47 +1,50 @@
+import React from 'react'
 import { css } from 'styled-components'
-import viewport from './viewport'
+import Media from '@kupibilet/react-media'
 
-export const dimensions = {
-  mobile: { min: 0, max: 599 },
-  tablet: { min: 600, max: 999 },
-  handheld: { min: 0, max: 999 },
-  desktop: { min: 1000, max: Number.POSITIVE_INFINITY },
+const queries = {
+  isMobile: 'screen and (max-width: 599px)',
+  isTablet: 'screen and (min-width: 600px) and (max-width: 999px)',
+  isHandheld: 'screen and (max-width: 999px)',
+  isDesktop: 'screen and (min-width: 1000px)',
 }
 
-export const isMobile = (width = viewport.getWidth()) => (
-  dimensions.mobile.min <= width && width <= dimensions.mobile.max
-)
-export const isTablet = (width = viewport.getWidth()) => (
-  dimensions.tablet.min <= width && width <= dimensions.tablet.max
-)
-export const isHandheld = (width = viewport.getWidth()) => (
-  dimensions.handheld.min <= width && width <= dimensions.handheld.max
-)
-export const isDesktop = (width = viewport.getWidth()) => (
-  dimensions.desktop.min <= width && width <= dimensions.desktop.max
+
+/**
+ * @usage
+ * const Component = ({ isMobile, isTablet, isHandheld, isDesktop }) => {}
+ * export default connectMedia(Component)
+ */
+export const connectMedia = (Component) => (
+  function connectedMedia(props) {
+    return (
+      <Media queries={queries}>
+        { (medias) => (
+          <Component {...props} {...medias} />
+        ) }
+      </Media>
+    )
+  }
 )
 
 const media = {
   mobile: (...args) => css`
-    @media screen and (max-width: ${dimensions.mobile.max}px) {
+    @media ${queries.isMobile} {
       ${css(...args)}
     }
   `,
   tablet: (...args) => css`
-    @media screen and
-      (min-width: ${dimensions.tablet.min}px) and
-      (max-width: ${dimensions.tablet.max}px)
-    {
+    @media ${queries.isTablet} {
       ${css(...args)}
     }
   `,
   handheld: (...args) => css`
-    @media screen and (max-width: ${dimensions.handheld.max}px) {
+    @media ${queries.isHandheld} {
       ${css(...args)}
     }
   `,
   desktop: (...args) => css`
-    @media screen and (min-width: ${dimensions.desktop.min}px) {
+    @media ${queries.isDesktop} {
       ${css(...args)}
     }
   `,
