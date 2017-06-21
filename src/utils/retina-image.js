703b1f37ecf2f83698aed8a512ba0d44ae379cae
skobets <sergey.kobets@kupibilet.ru>
2017-06-21 18:14:48 +0300

    add utils retinaImage

---
+++ src/utils/retina-image.js
@@ -0,0 +1,10 @@
+import { hiDPI } from 'polished'
+
+export default (filename, filenameRetina) => (
+  {
+    backgroundImage: `url(${filename})`,
+    [hiDPI(1.5)]: {
+      backgroundImage: `url(${filenameRetina})`,
+    },
+  }
+)
