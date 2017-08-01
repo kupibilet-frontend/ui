62e0a4bac8c8e98c7c278932566a1c04b04804c9
NeZeD [Mac Air] <nezed@ya.ru>
2017-08-01 17:01:58 +0300

    Fix dirnames

---
+++ src/components/ControlsGroup/styled.js
@@ -0,0 +1,61 @@
+import styled from 'styled-components'
+import { transparentize } from 'polished'
+import Icon from 'components/Icon'
+import { switchTransition } from 'utils/transitions'
+
+const fadeGradient = ({ theme }) => (
+  /* eslint-disable prefer-template */
+  'linear-gradient(' +
+    '180deg, ' +
+    transparentize(1, theme.color.background) + ' 0%, ' +
+    theme.color.background + ' 15%, ' +
+    theme.color.background + ' 85%, ' +
+    transparentize(1, theme.color.background) + ' 100%' +
+  ')'
+)
+
+export const FlexContainer = styled.div`
+  display: flex;
+
+  & .controls-group__swap {
+    ${switchTransition}
+  }
+
+  &:hover .controls-group__swap {
+    opacity: 1;
+    transition-delay: .15s;
+  }
+`
+
+export const SwapContainer = styled.div`
+  position: relative;
+  align-self: stretch;
+  width: 0;
+
+  cursor: pointer;
+
+  .icon-inherit-color {
+    fill: ${({ theme }) => theme.color.miscDark};
+  }
+
+  &:hover .icon-inherit-color {
+    fill: ${({ theme }) => theme.color.primary};
+  }
+`
+
+export const SwapIcon = styled(Icon)`
+  box-sizing: content-box;
+  width: 15px;
+  padding: 5px 0;
+
+  position: absolute;
+  left: -7.5px;
+  top: 50%;
+  margin-top: -12.5px;
+  z-index: 3;
+  transfrom: translateZ(0);
+
+  background: ${(props) => fadeGradient(props)};
+
+  opacity: 0;
+`
---
+++ src/components/Dropdown/stories.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+import { storiesOf } from '@storybook/react'
+import { boolean } from '@storybook/addon-knobs'
+import updateKnob from 'utils/updateKnob'
+import Dropdown, { DropdownContent } from 'components/Dropdown'
+import Button from 'components/Button'
+
+const StyledDropdown = styled(Dropdown)`
+  ${DropdownContent} {
+    left: auto;
+    width: 300px;
+  }
+`
+
+storiesOf('Dropdown', module)
+  .addWithInfo('Defalut', () => {
+    const isOpen = boolean('isOpen', false)
+    const onToggle = (event, value) => {
+      event.preventDefault()
+      updateKnob('isOpen', 'boolean', value)
+    }
+
+    return (
+      <StyledDropdown
+        overlay={<div><h2>Tethered</h2></div>}
+        onToggle={onToggle}
+        isOpen={isOpen}
+      >
+        <div>
+          click to open
+          &nbsp;
+          <Button>click</Button>
+        </div>
+      </StyledDropdown>
+    )
+  })
