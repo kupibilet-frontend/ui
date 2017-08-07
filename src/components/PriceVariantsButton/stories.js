95955829fc36a00c6c0a748ef6e3dc8f084fa06e
NeZeD [Mac Air] <nezed@ya.ru>
2017-08-07 18:06:53 +0300

    Fix font and PriceVariantsButton

--- src/components/PriceVariantsButton/stories.js
+++ src/components/PriceVariantsButton/stories.js
@@ -1,56 +1,56 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { text, boolean } from '@storybook/addon-knobs'
+import { text, number } from '@storybook/addon-knobs'
 import PriceVariantsButton from 'components/PriceVariantsButton'
 import updateKnob from 'utils/updateKnob'
 
-const defaultChecked = {
+const defaultValues = {
   title: 'Эконом Базовый',
   description: 'Описание',
   price: '12 234 ₽',
   topIcon: ['plane'],
   bottomIcon: ['hand-baggage', 'exchange', 'return'],
-  active: false,
+  active: 1,
 }
 
+const onClick = (index) => updateKnob('active', 'number', index)
+
+/* eslint-disable react/jsx-no-bind */
 storiesOf('PriceVariantsButton', module)
   .addWithInfo('default', () => {
-    const title = text('title', defaultChecked.title)
-    const description = text('description', defaultChecked.description)
-    const price = text('price', defaultChecked.price)
-    const active = boolean('active', defaultChecked.active)
-    const onClick = (flag) => {
-      updateKnob('active', 'boolean', flag)
-    }
+    const title = text('title', defaultValues.title)
+    const description = text('description', defaultValues.description)
+    const price = text('price', defaultValues.price)
+    const active = number('active', defaultValues.active)
 
     return (
       <div>
         <PriceVariantsButton
-          active={!active}
+          active={active === 1}
           title="Банковской картой он-лайн"
           price={price}
           iconsPosition="top"
-          icons={defaultChecked.topIcon}
+          icons={defaultValues.topIcon}
           description={description}
-          onClick={onClick}
+          onChange={onClick.bind(null, 1)}
         />
         <PriceVariantsButton
-          active={active}
+          active={active === 2}
           title={title}
           price={price}
           iconsPosition="bottom"
-          icons={defaultChecked.bottomIcon}
+          icons={defaultValues.bottomIcon}
           description={description}
-          onClick={onClick}
+          onChange={onClick.bind(null, 2)}
         />
         <PriceVariantsButton
-          active={active}
+          active={active === 3}
           title="Эконом"
           price={price}
           iconsPosition="bottom"
-          icons={defaultChecked.bottomIcon}
+          icons={defaultValues.bottomIcon}
           description={description}
-          onClick={onClick}
+          onChange={onClick.bind(null, 3)}
         />
       </div>
     )
