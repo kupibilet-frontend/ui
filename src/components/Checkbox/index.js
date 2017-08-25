59364f24debc2c123154c5bd6e8b791e279e7844
NeZeD [Mac Air] <nezed@ya.ru>
2017-08-25 19:41:20 +0300

    Redux-form demo with Checkbox

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -4,7 +4,7 @@ import Icon from 'components/Icon'
 import { CheckboxLabel, StyledCheckbox, IconWrap, CheckboxInput, LabelText } from './styled'
 
 
-export default class Checkbox extends PureComponent {
+export class Checkbox extends PureComponent {
   static propTypes = {
     onChange: PropTypes.func.isRequired,
     checked: PropTypes.bool.isRequired,
@@ -62,6 +62,29 @@ export default class Checkbox extends PureComponent {
         </LabelText>
 
       </CheckboxLabel>
+    )
+  }
+}
+
+// Disable eslint error: "Declare only one React component per file"
+// eslint-disable-next-line react/no-multi-comp
+export default class RFCheckbox extends React.PureComponent {
+  // Ignore RF checkbox behaviour due true/"" values instead of expected true/false
+  // See https://github.com/erikras/redux-form/pull/2863 and https://git.io/vHlZn
+  onChange = (e) => (
+    this.props.input.onChange(e.target.checked)
+  )
+
+  render() {
+    const { input, type = 'checkbox', ...props } = this.props
+
+    return (
+      <Checkbox
+        {...props}
+        {...input}
+        type={type}
+        onChange={this.onChange}
+      />
     )
   }
 }
