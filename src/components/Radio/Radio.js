2b5cc7cef90390986a1770c86e346580fc8e2bc2
Ilya Nesterov <i.nesterov@kupibilet.ru>
2017-09-05 12:02:20 +0300

    Fix radio button

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -1,10 +1,95 @@
 // @flow
 
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import { RadioLabel, StyledRadio, LabelText, RadioInput } from './styled'
+import { switchTransition } from 'utils/transitions'
 
 export type Value = string | boolean | number;
+
+const getBorderColor = (props) => {
+  const { theme } = props
+
+  if (props.checked) {
+    return theme.color.primary
+  } else if (props.disabled) {
+    return theme.color.textLighter
+  }
+
+  return theme.color.misc
+}
+
+const getBoxShadow = (props) =>
+  `box-shadow: inset 0 0 0 1px ${getBorderColor(props)}`
+
+const getRadioBackground = (props) => {
+  const { theme } = props
+
+  if (props.checked) {
+    return theme.color.primary
+  } else if (props.disabled) {
+    return theme.color.textLightest
+  }
+
+  return theme.color.background
+}
+
+export const RadioInput = styled.input`
+  display: none;
+`
+
+export const StyledRadio = styled.div`
+  flex-shrink: 0;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  ${getBoxShadow};
+
+  ${switchTransition};
+  transition-property: background, border;
+  background: ${getRadioBackground};
+
+  &:after {
+    content: ' ';
+    width: 8px;
+    height: 8px;
+    background: ${({ theme }) => theme.color.background};
+    display: inline-flex;
+    border-radius: 50%;
+    opacity: ${({ checked }) => (checked ? 1 : 0)}
+  };
+`
+
+export const LabelText = styled.span`
+  ${switchTransition};
+  transition-property: color;
+  margin-left: 6px;
+  width: 100%;
+  ${({ disabled, theme }) => (disabled &&
+    `color: ${theme.color.textLight};`
+  )}
+`
+
+export const RadioLabel = styled.label`
+  cursor: ${(props) => (props.disabled ? 'default' : 'pointer')};
+  display: inline-flex;
+  align-items: center;
+  position: relative;
+  user-select: none;
+  width: 100%;
+
+  ${StyledRadio}:hover {
+    border-color: ${({ theme, disabled }) => (disabled ? theme.color.textLighter : theme.color.primary)};
+  };
+
+  ${LabelText}:hover {
+    color: ${({ theme, disabled }) => (disabled ? theme.color.textLight : theme.color.primaryDarkest)};
+  };
+`
 
 type Props = {
   value: Value;
