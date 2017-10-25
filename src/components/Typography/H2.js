9f332decae2c0334871904aa4d270a448231fe85
Ksenia <ksenia.guba@kupibilet.ru>
2017-10-25 12:40:42 +0300

    add media-queries and  adaptive styles

--- src/components/Typography/H2.js
+++ src/components/Typography/H2.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import mq from 'utils/media-queries'
 
 const H2 = styled.h2`
   font-size: 38px;
@@ -6,6 +7,16 @@ const H2 = styled.h2`
   line-height: 38px;
   letter-spacing: -1px;
   margin: 0;
+  ${mq.handheld`
+    font-size: 30px;
+    line-height: 34px;
+    letter-spacing: -.6px;
+  `}
+  ${mq.mobile`
+    font-size: 30px;
+    line-height: 34px;
+    letter-spacing: -.6px;
+  `}
 `
 
 export default H2
