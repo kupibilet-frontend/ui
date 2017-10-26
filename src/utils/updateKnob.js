d0ef3122eab3ee5bfd79aebc8b033dab8fe3ceab
NeZeD [Mac Air] <nezed@ya.ru>
2017-10-26 19:17:31 +0300

    Drop warning about moving `src/utils/updateKnob.js` to `storybook/updateKnob.js`

--- src/utils/updateKnob.js
+++
@@ -1,6 +0,0 @@
-export { default } from 'storybook/updateKnob'
-
-// TODO remove this file
-console.error(`\`updateKnob\` was moved and should be imported as
-> import updateKnob from 'storybook/updateKnob';
-`)
