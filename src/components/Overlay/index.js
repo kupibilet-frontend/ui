2abd72db705e044619f685785863e2df5662bba0
mr-aibeck <mr.aibeck@gmail.com>
2017-11-29 18:05:51 +0300

    Feature/full screen component (#167)
    * full screen component
    
    * key for array
    
    * snapshot
    
    * fixes
    
    * fixes
    
    * 3.3.5-0
    
    * snapshot
    
    * 3.3.5-1
    
    * 3.3.5-2
    
    * fix for onclick
    
    * 3.3.5-3
    
    * fix for z-index
    
    * 3.3.5-4
    
    * 3.3.5-5
    
    * 3.3.5-6
    
    * fixes
    
    * add desktop, handheld & mobile modal component
    
    * update snapshot
    
    * fix
    
    * fix for overlay
    
    * fixes & overlay component
    
    * fix
    
    * snapshot
    
    * 3.3.5-7
    
    * fix for full screen
    
    * 3.3.5-8
    
    * add snapshot
    
    * fixes for modal
    
    * fix
    
    * snapshot
    
    * update snapshot
    
    * fix
    
    * fixes for pr
    
    * update snapshot
    
    * fixes for modal
    
    * fix for footer
    
    * Cleanup story
    
    * fix for snapshot
    
    * Dima sdelal svou huinu kak hotel'

---
+++ src/components/Overlay/index.js
@@ -0,0 +1,85 @@
+// @flow
+import React from 'react'
+import GlobalStylesScope from 'components/ThemeProvider'
+import Icon from 'components/Icon'
+import { withMedia } from 'utils/media-queries'
+
+import {
+  Wrapper,
+  OverlayContentWrap,
+  OverlayCell,
+  OverlayContent,
+  OverlayClosePanel,
+  OverlayClosePanelIcon,
+  BackLink,
+} from './styled'
+
+type Props = {
+  closePortal?: (Event) => void,
+  isDesktop: boolean,
+  backLink: string,
+  children: React.Element<*>,
+}
+
+class Overlay extends React.Component<{}, Props, void> {
+  static defaultProps = {
+    backLink: '',
+  }
+
+  componentWillMount() {
+    this.calcWidth()
+    window.addEventListener('resize', this.calcWidth)
+  }
+
+  componentDidMount() {
+    document.body.style.overflow = 'hidden'
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.calcWidth)
+    document.body.style.overflow = 'auto'
+    document.body.style.maxWidth = 'none'
+  }
+
+  stopPropagation = (e) => {
+    e.stopPropagation()
+  }
+
+  calcWidth = () => {
+    document.body.style.maxWidth = `${document.body.clientWidth}px`
+  }
+
+  render() {
+    return (
+      <GlobalStylesScope>
+        <Wrapper
+          onClick={this.props.closePortal}
+        >
+          <OverlayContentWrap>
+            <OverlayCell>
+              {!this.props.isDesktop &&
+                <OverlayClosePanel>
+                  {this.props.backLink &&
+                    <BackLink>
+                      <Icon name="arrow-down" fill="primaryDarkest" size="xxsmall" className="arrow-icon" />
+                      &nbsp;{this.props.backLink}
+                    </BackLink>
+                  }
+                  <OverlayClosePanelIcon>
+                    <Icon name="cross" fill="primaryDarkest" size="xxsmall" className="closing-icon" />
+                  </OverlayClosePanelIcon>
+                </OverlayClosePanel>
+              }
+              <OverlayContent onClick={this.stopPropagation}>
+                {React.cloneElement(this.props.children, { closePortal: this.props.closePortal })}
+              </OverlayContent>
+            </OverlayCell>
+          </OverlayContentWrap>
+        </Wrapper>
+      </GlobalStylesScope>
+    )
+  }
+}
+
+
+export default withMedia(Overlay)
