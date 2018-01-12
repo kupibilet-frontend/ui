c531773a3c969a91f41a0d13f6d59be076bdbfca
Ksenia <ksenia.guba@kupibilet.ru>
2018-01-12 15:02:55 +0300

    remove display initial

--- src/components/ResponsiveOnly/index.js
+++ src/components/ResponsiveOnly/index.js
@@ -2,36 +2,34 @@ import styled from 'styled-components'
 import mq from 'utils/media-queries'
 
 const DesktopOnly = styled.div`
-  display: none;
-
-  ${mq.desktop`
-    display: initial;
+  ${mq.handheld`
+    display: none;
   `}
 `
 DesktopOnly.displayName = 'DesktopOnly'
 
 const MobileOnly = styled.div`
-  display: none;
-
-  ${mq.mobile`
-    display: initial;
+  ${mq.desktop`
+    display: none;
+  `}
+  ${mq.tablet`
+    display: none;
   `}
 `
 
 const TabletOnly = styled.div`
-  display: none;
-
-  ${mq.tablet`
-    display: initial;
+  ${mq.desktop`
+    display: none;
+  `}
+  ${mq.mobile`
+    display: none;
   `}
 `
 TabletOnly.displayName = 'TabletOnly'
 
 const HandheldOnly = styled.div`
-  display: none;
-
-  ${mq.handheld`
-    display: initial;
+  ${mq.desktop`
+    display: none;
   `}
 `
 HandheldOnly.displayName = 'HandheldOnly'
