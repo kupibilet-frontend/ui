24aa760cfabd2acc0b7be18ec712fec3e6ef23a8
Ksenia <ksenia.guba@kupibilet.ru>
2018-04-05 13:22:31 +0300

    new way to stop scrolling window

--- src/components/Overlay/index.js
+++ src/components/Overlay/index.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 import type { Element } from 'react'
 import GlobalStylesScope from 'components/ThemeProvider'
 import { withMedia } from 'utils/media-queries'
-import Scrollfix from 'components/Scrollfix'
 
 import {
   Wrapper,
@@ -15,10 +14,52 @@ type Props = {
   closePortal?: (Event) => void,
   isOnBottom: boolean,
   children: Element<*>,
-  freezableElement: string,
 }
 
 class Overlay extends Component<Props, void> {
+  componentDidMount() {
+    this.element.addEventListener('mousewheel', this.onWheel, true)
+    this.element.addEventListener('touchstart', this.getTouchStartCoord, true)
+    this.element.addEventListener('touchmove', this.onTouch, true)
+  }
+
+  componentWillUnmount() {
+    this.element.removeEventListener('mousewheel', this.onWheel, true)
+    this.element.removeEventListener('touchstart', this.getTouchStartCoord, true)
+    this.element.removeEventListener('touchmove', this.onTouch, true)
+  }
+
+  onWheel = (event) => {
+    const { deltaY } = event
+    this.onScroll(event, deltaY)
+  }
+
+  onTouch = (event) => {
+    const currentY = event.changedTouches[0].clientY
+    const deltaY = this.lastY - currentY
+    this.onScroll(event, deltaY)
+  }
+
+  onScroll = (event, deltaY) => {
+    const scrollTop = this.element.scrollTop
+    if (scrollTop <= 1 && deltaY < 0) {
+      event.preventDefault()
+    } else if (
+      scrollTop + this.element.offsetHeight >= this.element.scrollHeight - 1
+      && deltaY > 0
+    ) {
+      event.preventDefault()
+    }
+  }
+
+  getTouchStartCoord = (event) => {
+    this.lastY = event.changedTouches[0].clientY
+  }
+
+  element = {}
+  lastY = 0
+
+
   stopPropagation = (e) => {
     e.stopPropagation()
   }
@@ -28,24 +69,22 @@ class Overlay extends Component<Props, void> {
       closePortal,
       isOnBottom,
       children,
-      freezableElement,
     } = this.props
 
     return (
-      <Scrollfix freezableElement={freezableElement}>
-        <GlobalStylesScope>
-          <Wrapper
-            onClick={closePortal}
-            isOnBottom={isOnBottom}
-          >
-            <OverlayContentWrap isOnBottom={isOnBottom}>
-              <OverlayContent onClick={this.stopPropagation} isOnBottom={isOnBottom}>
-                {React.cloneElement(children, { closePortal })}
-              </OverlayContent>
-            </OverlayContentWrap>
-          </Wrapper>
-        </GlobalStylesScope>
-      </Scrollfix>
+      <GlobalStylesScope>
+        <Wrapper
+          onClick={closePortal}
+          isOnBottom={isOnBottom}
+          innerRef={(element) => { this.element = element }}
+        >
+          <OverlayContentWrap isOnBottom={isOnBottom}>
+            <OverlayContent onClick={this.stopPropagation} isOnBottom={isOnBottom}>
+              {React.cloneElement(children, { closePortal })}
+            </OverlayContent>
+          </OverlayContentWrap>
+        </Wrapper>
+      </GlobalStylesScope>
     )
   }
 }
