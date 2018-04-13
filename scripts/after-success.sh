ee35a1542c2d8136a075ba5064cb8875d70f75b9
Dmitry Naumov <nezed@ya.ru>
2018-04-13 20:20:03 +0300

    Fix versionize missing publish (#241)

--- scripts/after-success.sh
+++ scripts/after-success.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-yarn versionize
-
-VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
-COMMENT="New version of package: \`$VERSION\`"
-
-
 if [ "$TRAVIS_PULL_REQUEST" ]; then
-  curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+  yarn versionize
+
+  VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
+  COMMENT="New version of package: \`$VERSION\`"
+
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+  npm publish --tag dev
+  curl \
+    -sS \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    -X POST \
+    -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
