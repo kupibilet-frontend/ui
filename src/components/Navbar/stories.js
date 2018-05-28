e7c3171ab1474b06067f4d6305e3f18dd5203ffb
Vladimir Cibizov <vladimir.tsibizow@gmail.com>
2018-05-28 18:02:42 +0300

    Added Navbar

---
+++ src/components/Navbar/stories.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import Navbar, { Item } from 'components/Navbar'
+
+const itemList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+class Wrap extends React.Component {
+  state = {
+    isActive: 0,
+  }
+
+  onClick = (i) => { this.setState({ isActive: i }) }
+
+  render() {
+    return (
+      <Navbar>
+        {itemList.map((item, i) => (
+          <Item isActive={this.state.isActive === i} onClick={() => this.onClick(i)}>{item}</Item>
+        ))}
+      </Navbar>
+    )
+  }
+}
+
+
+storiesOf('Blocks', module)
+  .addWithInfo('Navbar', () => <Wrap />)
