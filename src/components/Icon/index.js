1def99d359717f4df92b455489d475a171af9fe9
Vladimir Cibizov <vladimir.tsibizow@gmail.com>
2018-05-29 17:59:45 +0300

    Added animation for icon spinner (#256)
    * Added animation for icon spinner
    
    * Change to keyframes
    
    * Change to keyframes

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { withTheme } from 'styled-components'
+import styled, { withTheme, keyframes } from 'styled-components'
 import cn from 'classnames'
 
 import { switchTransition } from 'utils/transitions'
@@ -17,6 +17,26 @@ export const sizes = {
   xxlarge: 60,
 }
 
+const preloader = keyframes`
+  0% { transform: rotate(0deg); }
+
+  6.6%  { transform: rotate(50deg);   }
+  16.6% { transform: rotate(300deg);  }
+  23.4% { transform: rotate(350deg);  }
+  33.4% { transform: rotate(600deg);  }
+  40%   { transform: rotate(650deg);  }
+
+  50%   { transform: rotate(900deg);  }
+
+  56.6% { transform: rotate(950deg);  }
+  66.6% { transform: rotate(1200deg); }
+  73.4% { transform: rotate(1250deg); }
+  83.4% { transform: rotate(1500deg); }
+  90%   { transform: rotate(1550deg); }
+
+  100%  { transform: rotate(1800deg); }
+`
+
 const IconSvg = styled.svg`
   ${switchTransition};
   transition-property: fill, transform;
@@ -32,6 +52,10 @@ const IconSvg = styled.svg`
       `
     }
   }}
+
+  ${({ name }) => name === 'spinner' && `
+    animation: ${preloader} 3s infinite ease-in-out;
+  `}
 `
 
 export const IconWrapper = styled.span`
@@ -62,6 +86,7 @@ const Icon = ({ prefix, name, striked, className, inheritColor, rotate, ...props
   <IconWrapper striked={striked}>
     <IconSvg
       {...props}
+      name={name}
       rotate={rotate ? 'true' : ''}
       striked={striked}
       className={cn(className, { 'icon-inherit-color': inheritColor })}
