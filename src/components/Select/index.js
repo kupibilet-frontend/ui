6aae4d14887a8341aea58bd2d8a4772b285eaa68
Ilya Nesterov <iljanest@yandex.ru>
2018-07-20 16:06:35 +0300

    Fixed responsive select

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -5,13 +5,17 @@ import Autocomplete from 'components/Autocomplete'
 import RFInput, { Input } from 'components/Input'
 import Icon from 'components/Icon'
 import Suggestion from 'components/Suggestion'
+import { shadowSmall } from 'utils/shadows'
+import mq from 'utils/media-queries'
 import { SuggestionsContainer } from 'components/Autocomplete/styled'
 
 
 const StyledContainer = styled(SuggestionsContainer)`
   min-width: 90px;
   width: 100%;
-  position: relative;
+  ${mq.mobile`
+    ${shadowSmall};
+  `}
 `
 
 const SectionHeader = styled.div`
