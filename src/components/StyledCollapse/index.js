7598f575b4bccea58157b6cb65985b22c77ecc86
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2019-01-14 17:52:31 +0300

    Add StyledCollapse component to unify styles of Collapse component

---
+++ src/components/StyledCollapse/index.js
@@ -0,0 +1,51 @@
+/*
+ * StyledCollapse is just a simple wrapper of Collapse component
+ * It is used to unify collapse elements in the company
+ * */
+import React from 'react'
+import styled from 'styled-components'
+import Icon from 'components/Icon'
+import StyledCollapse from 'components/Collapse'
+
+
+const PanelHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 12px ${({ withPadding }) => (withPadding ? '18px' : '0')};
+  width: 100%;
+  border-bottom: 1px solid ${({ theme }) => theme.color.miscLighter}
+`
+
+const PanelHeaderText = styled.div`
+  font-weight: 700;
+  user-select: none;
+`
+
+export const CollapseHeaderArrow = styled(Icon)`
+  fill: ${({ theme }) => theme.color.miscDarker};
+  display: block;
+`
+
+const PanelContentWrapper = styled.div`
+  padding: 0 ${({ withPadding }) => (withPadding ? '18px' : '0')};
+`
+
+export const StyledPanel = (props) => (
+  <StyledCollapse.Panel
+    {...props}
+    header={props.renderHeader ? props.renderHeader() : (
+      <PanelHeader withPadding={props.withPadding}>
+        <PanelHeaderText>{props.header}</PanelHeaderText>
+        <CollapseHeaderArrow name="angle" rotate={props.isActive} size="normal" />
+      </PanelHeader>
+    )}
+  >
+    <PanelContentWrapper withPadding={props.withPadding}>
+      {props.children}
+    </PanelContentWrapper>
+  </StyledCollapse.Panel>
+)
+
+export default StyledCollapse
