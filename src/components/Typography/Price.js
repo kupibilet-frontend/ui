424fb098745ea898a15c7f78e4d1cd117a574242
Ev Bogdanov <evbogdanov.com@gmail.com>
2019-01-16 15:08:48 +0300

    Add Price component

---
+++ src/components/Typography/Price.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Price = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  line-height: 22px;
+  color: ${({ theme }) => theme.color.secondaryDarker};
+`
+
+export default Price
