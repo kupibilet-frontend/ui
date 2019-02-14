d3670f57ab194e0121d1399078181982daacdd6f
Ev Bogdanov <evbogdanov.com@gmail.com>
2019-02-14 15:29:07 +0300

    Add Header component (#324)
    * Add Header component
    
    * Make ESLint happy
    
    * Get rid of unnecessary props
    
    * Update snapshots
    
    * Simplify everything

---
+++ src/components/Header/styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import mq from 'utils/media-queries'
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+  background: ${({ theme }) => theme.color.background};
+  border-bottom: 1px solid ${({ theme }) => theme.color.miscLighter};
+`
+
+export const HeaderInner = styled.div`
+  max-width: 1178px;
+  height: 48px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  ${mq.handheld`
+    padding: 0 18px;
+  `}
+`
+
+export const HeaderContent = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
