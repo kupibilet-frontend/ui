dab134f0c770005dfb0f51ed63861780c0b4aafa
Nikita Melnikov <nikita.melnikov@kupibilet.ru>
2019-03-07 13:57:56 +0300

    add test for undef id

--- src/utils/createTestId.js
+++ src/utils/createTestId.js
@@ -1,4 +1,5 @@
 const createTestId = (id, additionalParams = {}) => {
+  if (!id || id === undefined) return
   const additionalKeys = Object.keys(additionalParams)
   const paramsArray = additionalKeys.length > 0 ?
     additionalKeys.map((item) => {
