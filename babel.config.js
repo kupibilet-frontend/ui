6ccbdc1ae28460a98f5d96a5e6527d9f2f465f9b
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2019-05-27 17:14:19 +0300

    Configure babel and jest to work with `require.context`
    After upgrade to Storybook 4-5 versions, tests started to fall because `require.context`
    is no longer supported in jest, so we need to make the following steps to read storybook config in test environment:
    1) Add babel plugin to transpile `require.context` to `__requireContext` only in test env
    2) Add setup script for jest to register function `__requireContext`
    
    I have noticed a lot of rule duplucation in .babelrc file, so I refactored it to js config

---
+++ babel.config.js
@@ -0,0 +1,52 @@
+const BASE_PLUGINS = [
+  ["flow-react-proptypes"],
+  ["@babel/plugin-proposal-class-properties"],
+  ["@babel/plugin-proposal-export-default-from"],
+  ["module-resolver", {
+    "root": ["./"],
+    "alias": {
+      "blocks": "./src/blocks",
+      "components": "./src/components",
+      "utils": "./src/utils",
+      "storybook": "./storybook"
+    }
+  }],
+]
+
+const BASE_PRESETS = ["@babel/preset-env", "@babel/preset-react", "@babel/preset-flow"]
+
+module.exports = {
+  env: {
+    default: {
+      plugins: [
+        ...BASE_PLUGINS,
+        ["styled-components", {
+          "minify": false,
+        }],
+      ],
+      presets: BASE_PRESETS,
+    },
+
+    esmodules: {
+      plugins: [
+        ...BASE_PLUGINS,
+        ["styled-components", {
+          "ssr": true
+        }]
+      ],
+      presets: BASE_PRESETS,
+    },
+
+    test: {
+      plugins: [
+        ...BASE_PLUGINS,
+        ["styled-components", {
+          "minify": false,
+        }],
+        ["require-context-hook"],
+      ],
+      presets: BASE_PRESETS,
+    }
+  }
+}
+
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
-// `jest` doesn't work without this config, see GitHub issue:
-// https://github.com/facebook/jest/issues/6766
 module.exports = {
+  // `jest` doesn't work without this config, see GitHub issue:
+  // https://github.com/facebook/jest/issues/6766
   testURL: 'http://localhost',
+
+  // Configure Jest to work with Webpack's require.context()
+  // https://github.com/storybooks/storybook/tree/master/addons/storyshots/storyshots-core#configure-jest-to-work-with-webpacks-requirecontext
+  setupFiles: ['<rootDir>/.jest/register-context.js'],
 }
