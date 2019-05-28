d33f9e2a75edf13d9cb28e1b93088d6ea8c0fa5c
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2019-05-28 18:07:19 +0300

    Add viewport storybook addon for viewing component on different screen sizes

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,5 @@
 import '@storybook/addon-storysource/register'
+import '@storybook/addon-viewport/register'
 import '@storybook/addon-knobs/register'
 import 'storybook/addons/updateKnob'
 // import 'storybook-addon-redux-listener/register'
