146955e15d07b7e8481ca5e533e4ea0bc73f8437
Dmitry Naumov <nezed@ya.ru>
2019-05-31 00:04:58 +0300

    Move box story

--- src/components/Box/stories.js
+++ src/components/Box/stories.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import Box from 'components/Box'
 
-storiesOf('COMPONENTS|Box', module)
+storiesOf('COMPONENTS|Layout/Box', module)
   .add('Default', () => (
     <Box>
       <p>box content</p>
