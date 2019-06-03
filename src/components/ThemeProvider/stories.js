bc6937d67fd17bbc9464ffecedd2b4b3cc47764d
Nikita Melnikov <nikita.melnikov@kupibilet.ru>
2019-06-03 14:04:13 +0300

    add colors to storybook

---
+++ src/components/ThemeProvider/stories.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+import { storiesOf } from '@storybook/react'
+import { color } from './theme'
+
+const ColorBox = styled.div`
+  width: 90px;
+  height: 50px;
+  background-color: ${({ theme, pickedColor }) => theme.color[pickedColor]};
+  padding: 10px;
+  margin: 5px;
+`
+const ColorCard = styled.div`
+  flex: 1;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const ColorPanel = styled.div`
+  display: flex;
+  width: 900px;
+  flex-wrap: wrap;
+`
+const colors = Object.keys(color)
+
+storiesOf('COLORS', module)
+  .add('Theme colors', () => (
+    <ColorPanel>
+      {colors.map((pickedColor) => (
+        <ColorCard>
+          <span>{pickedColor} - {color[pickedColor]}</span>
+          <ColorBox pickedColor={pickedColor} />
+        </ColorCard>
+      ))}
+    </ColorPanel>
+  ))
