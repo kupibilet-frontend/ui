18ed5a4cd1c816c2cf8c5846040087df0145571a
u.saltrukovich <yulya_9060@mail.ru>
2019-06-05 16:02:36 +0300

    add babel-plugin-import

--- babel.config.js
+++ babel.config.js
@@ -1,27 +1,30 @@
 const BASE_PLUGINS = [
-  ["flow-react-proptypes"],
-  ["@babel/plugin-proposal-class-properties"],
-  ["@babel/plugin-proposal-export-default-from"],
-  ["module-resolver", {
-    "root": ["./"],
-    "alias": {
-      "blocks": "./src/blocks",
-      "components": "./src/components",
-      "utils": "./src/utils",
-      "storybook": "./storybook"
-    }
+  ['flow-react-proptypes'],
+  ['@babel/plugin-proposal-class-properties'],
+  ['@babel/plugin-proposal-export-default-from'],
+  ['module-resolver', {
+    root: ['./'],
+    alias: {
+      blocks: './src/blocks',
+      components: './src/components',
+      utils: './src/utils',
+      storybook: './storybook',
+    },
+  }],
+  ['import', {
+    libraryName: '@kupibilet/ui/components',
   }],
 ]
 
-const BASE_PRESETS = ["@babel/preset-env", "@babel/preset-react", "@babel/preset-flow"]
+const BASE_PRESETS = ['@babel/preset-env', '@babel/preset-react', '@babel/preset-flow']
 
 module.exports = {
   env: {
     default: {
       plugins: [
         ...BASE_PLUGINS,
-        ["styled-components", {
-          "minify": false,
+        ['styled-components', {
+          minify: false,
         }],
       ],
       presets: BASE_PRESETS,
@@ -30,9 +33,9 @@ module.exports = {
     esmodules: {
       plugins: [
         ...BASE_PLUGINS,
-        ["styled-components", {
-          "ssr": true
-        }]
+        ['styled-components', {
+          ssr: true,
+        }],
       ],
       presets: BASE_PRESETS,
     },
@@ -40,13 +43,12 @@ module.exports = {
     test: {
       plugins: [
         ...BASE_PLUGINS,
-        ["styled-components", {
-          "minify": false,
+        ['styled-components', {
+          minify: false,
         }],
-        ["require-context-hook"],
+        ['require-context-hook'],
       ],
       presets: BASE_PRESETS,
-    }
-  }
+    },
+  },
 }
-
