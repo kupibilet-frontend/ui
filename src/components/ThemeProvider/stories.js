fd8096e2ecfc7cbcd758c993040dbe05e739c91e
sylenien <sylenien@gmail.com>
2019-06-06 18:23:57 +0300

    Add themecolors to storybook (#343)
    * add colors to storybook
    
    * make cards wider
    
    * update snaps
    
    * prettify things
    
    * update snap
    
    * add keys

---
+++ src/components/ThemeProvider/stories.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from 'styled-components'
+import { storiesOf } from '@storybook/react'
+import { color } from './theme'
+
+const ColorBox = styled.div`
+  width: 100px;
+  height: 50px;
+  background-color: ${({ theme, pickedColor }) => theme.color[pickedColor]};
+  padding: 10px;
+  margin: 5px;
+`
+const ColorCard = styled.div`
+  flex: 1;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, .1);
+  border-radius: 6px;
+`
+const ColorPanel = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(7, 140px);
+  width: 1000px;
+`
+const colors = Object.keys(color)
+
+storiesOf('COLORS', module)
+  .add('Theme colors', () => (
+    <ColorPanel>
+      {colors.map((pickedColor) => (
+        <ColorCard
+          key={pickedColor}
+        >
+          <span>{pickedColor}</span>
+          <span>{color[pickedColor]}</span>
+          <ColorBox pickedColor={pickedColor} />
+        </ColorCard>
+      ))}
+    </ColorPanel>
+  ))
