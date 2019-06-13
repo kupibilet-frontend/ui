a718d0dd81e64a653061d585eda533c2cd603d13
Nikita Melnikov <nikita.melnikov@kupibilet.ru>
2019-06-13 14:55:33 +0300

    add c2c func and animations

--- src/components/ThemeProvider/stories.js
+++ src/components/ThemeProvider/stories.js
@@ -12,6 +12,7 @@ const ColorBox = styled.div`
 `
 const ColorCard = styled.div`
   flex: 1;
+  cursor: pointer;
   padding: 10px;
   display: flex;
   flex-direction: column;
@@ -19,26 +20,68 @@ const ColorCard = styled.div`
   align-items: center;
   border: 1px solid rgba(0, 0, 0, .1);
   border-radius: 6px;
+  box-shadow: 0 6px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: all 0.125s ease-in-out;
+  &:hover {
+    box-shadow: 0 0 2px rgba(0,0,0, 0.10);
+  }
+`
+const Text = styled.div`
+  width: 300px;
+  padding-top: 20px;
+  margin: 0 auto;
 `
 const ColorPanel = styled.div`
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 12px;
+  padding: 20px;
   grid-template-columns: repeat(7, 140px);
   width: 1000px;
 `
+
 const colors = Object.keys(color)
+
+const copyToClipboard = (str) => {
+  const el = document.createElement('textarea')
+  el.value = str
+  el.setAttribute('readonly', '')
+  el.style.position = 'absolute'
+  el.style.left = '-9999px'
+  document.body.appendChild(el)
+  const selected =
+    document.getSelection().rangeCount > 0
+      ? document.getSelection().getRangeAt(0)
+      : false
+  el.select()
+  document.execCommand('copy')
+  document.body.removeChild(el)
+  const card = document.getElementById(str)
+  card.style.backgroundColor = color[str]
+  setTimeout(() => {
+    card.style.backgroundColor = 'white'
+  }, 3000)
+  if (selected) {
+    document.getSelection().removeAllRanges()
+    document.getSelection().addRange(selected)
+  }
+}
 
 storiesOf('COLORS', module)
   .add('Theme colors', () => (
-    <ColorPanel>
-      {colors.map((pickedColor) => (
-        <ColorCard
-          key={pickedColor}
-        >
-          <span>{pickedColor}</span>
-          <span>{color[pickedColor]}</span>
-          <ColorBox pickedColor={pickedColor} />
-        </ColorCard>
-      ))}
-    </ColorPanel>
+    <>
+      <Text>click to copy</Text>
+      <ColorPanel>
+        {colors.map((pickedColor) => (
+          <ColorCard
+            id={pickedColor}
+            onClick={() => copyToClipboard(pickedColor)}
+            key={pickedColor}
+          >
+            <span>{pickedColor}</span>
+            <span>{color[pickedColor]}</span>
+            <ColorBox pickedColor={pickedColor} />
+          </ColorCard>
+        ))}
+      </ColorPanel>
+    </>
   ))
