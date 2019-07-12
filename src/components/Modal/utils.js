2c57f033268e9682e4a68fadecfc55679459543c
allthemwitches <dmitry.verteleckij@kupibilet.ru>
2019-07-12 17:27:28 +0300

    SITE-1630

--- src/components/Modal/utils.js
+++ src/components/Modal/utils.js
@@ -4,8 +4,4 @@ export const isThin = (string) => string === 'thin'
 
 export const isCompact = (string) => string === 'compact'
 
-export const isWide = (string) => string === 'wide'
-
-export const isSetSize = (string) => (
-  isThin(string) || isCompact(string) || isWide(string)
-)
+export const isSetSize = (string) => isThin(string) || isCompact(string)
