76e524e20dfa8169858188ed39e4b1e54af5901d
Nikita Melnikov <nikita.melnikov@kupibilet.ru>
2019-07-15 12:22:03 +0300

    add type

--- src/components/PriceVariantsButton/index.js
+++ src/components/PriceVariantsButton/index.js
@@ -66,7 +66,7 @@ const Title = styled.span`
 type Props = {
   active: boolean,
   onChange?: (Event) => void,
-  buttonTitle: string | Element,
+  buttonTitle: FormattedMessage,
   price: string | Element<*>,
   icons: Array<Element<*>>,
   iconsPosition?: string,
