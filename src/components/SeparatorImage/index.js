e2a1699c81069f9ad5785a2e67eb18f6623a17b1
Юлия Салтрукович <yulya_9060@mail.ru>
2019-07-29 17:02:50 +0300

    fix styles for Bubble, SeparatorImage (#371)

--- src/components/SeparatorImage/index.js
+++ src/components/SeparatorImage/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import mq from 'utils/media-queries'
 
 const Separator = styled.div`
   width: 100%;
@@ -9,10 +8,6 @@ const Separator = styled.div`
   background-repeat: no-repeat;
   background-image: ${({ image }) => `url("${image}")`};
   background-position: center center;
-  ${mq.mobile`
-   background-position: 79% 50%;
-   background-size: cover;
-  `}
 `
 
 const SeparatorImage = (props) => <Separator {...props} />
