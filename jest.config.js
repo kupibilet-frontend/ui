45d47570fa0655e59a8a788435ae9a8217bf4296
Ksenia <ksenia.guba@kupibilet.ru>
2019-12-05 14:29:19 +0300

    remove jest
    try without cache
    
    Add brand new script
    
    return all
    
    again
    
    add skiping script

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // `jest` doesn't work without this config, see GitHub issue:
-  // https://github.com/facebook/jest/issues/6766
-  testURL: 'http://localhost',
-
-  // Configure Jest to work with Webpack's require.context()
-  // https://github.com/storybooks/storybook/tree/master/addons/storyshots/storyshots-core#configure-jest-to-work-with-webpacks-requirecontext
-  setupFiles: ['<rootDir>/.jest/register-context.js'],
-}
