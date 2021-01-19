b20ad452ebb394d44b172b1118b1dc036802971a
Vladimir Nikitin <vladimir.nikitin@kupibilet.ru>
2021-01-19 15:44:20 +0300

    Add TypeScript support and migrate few files

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,34 @@
 const path = require('path')
 
 module.exports = function({ config }) {
+  config.resolve.extensions.push('.ts')
+  config.resolve.extensions.push('.tsx')
+
   config.module.rules.push({
-    test: /stories\.js$/,
+    test: /\.js$/,
     include: [path.resolve(__dirname, '../src')],
-    loaders: [require.resolve('@storybook/source-loader')],
+    loader: require.resolve('@storybook/source-loader'),
     enforce: 'pre',
   })
+
+  config.module.rules.push({
+    include: [path.resolve(__dirname, '../src')],
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [
+        '@babel/preset-env',
+        '@babel/preset-react',
+        '@babel/preset-typescript'
+      ]
+    }
+  })
+
+  config.resolve.alias = {
+    blocks: path.resolve(__dirname, '../src/blocks'),
+    components: path.resolve(__dirname, '../src/components'),
+    utils: path.resolve(__dirname, '../src/utils'),
+  }
 
   return config
 }
