7a78ebea63de91739619405ced829fce1224062f
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-03-31 13:28:04 +0300

    migrate Slider component to TypeScript (#487)
    * migrate Slider component to TypeScript
    
    * add disabled styles for Slider component

---
+++ src/components/Slider/index.tsx
@@ -0,0 +1,72 @@
+import 'rc-slider/assets/index.css'
+import React, { useEffect, useState } from 'react'
+import { HandleProps, Range } from 'rc-slider'
+import { StyledRangeWrapper, StyledHandleWrapper, StyledHandle } from './styled'
+
+
+interface TProps {
+  min?: number,
+  max?: number,
+  values?: number[],
+  disabled?: boolean,
+  handle?: () => React.ReactNode,
+  progressBar?: () => React.ReactNode,
+  onChange?: (values: number[]) => void,
+  onAfterChange?: (values: number[]) => void,
+}
+
+function getInitialValues(min: number, max: number, values?: number[]): number[] {
+  if (values) return values
+
+  return [min, max]
+}
+
+function Slider(props: TProps): JSX.Element {
+  const {
+    min = 1,
+    max = 100,
+    disabled = false,
+    onAfterChange = () => {}, // eslint-disable-line @typescript-eslint/no-empty-function
+  } = props
+
+  const [values, setValues] = useState<number[]>(getInitialValues(min, max, props.values))
+
+  const [firstValueFromProps, secondValueFromProps] = (props.values || [])
+
+  useEffect(() => {
+    const [firstCurrentValue, secondCurrentValue] = values
+
+    if (firstCurrentValue !== firstValueFromProps || secondCurrentValue !== secondValueFromProps) {
+      setValues([firstValueFromProps, secondValueFromProps])
+    }
+  }, [firstValueFromProps, secondValueFromProps])
+
+  function onChange(changedValues: number[]): void {
+    setValues(changedValues)
+
+    if (props.onChange) props.onChange(changedValues)
+  }
+
+  return (
+    <StyledRangeWrapper disabled={disabled}>
+      <Range
+        min={min}
+        max={max}
+        disabled={disabled}
+        allowCross={false}
+        defaultValue={values}
+        value={values}
+        onChange={onChange}
+        onAfterChange={onAfterChange}
+        // @ts-ignore some TS types doesn't match with rc-slider props
+        handle={(handleProps: HandleProps) => (
+          <StyledHandleWrapper {...handleProps} style={{ zIndex: 4, boxShadow: 'none' }}>
+            <StyledHandle min={min} max={max} value={handleProps.value} disabled={disabled} />
+          </StyledHandleWrapper>
+        )}
+      />
+    </StyledRangeWrapper>
+  )
+}
+
+export default Slider
---
+++ src/components/Slider/styled.ts
@@ -0,0 +1,81 @@
+import styled from 'styled-components'
+import { HandleProps, Handle } from 'rc-slider'
+import { TColor } from 'components/ThemeProvider/types'
+import { switchTransition } from 'utils/transitions'
+
+
+interface TStyledRangeWrapperProps {
+  disabled: boolean,
+}
+
+const StyledRangeWrapper = styled.div<TStyledRangeWrapperProps>`
+  .rc-slider-track {
+    background-color: ${({ theme, disabled }) => (
+    disabled ? theme.color.misc : theme.color.primaryDark
+  )};
+    display: flex;
+    margin-left: 1px;
+    position: absolute;
+    height: 6px;
+    top: 3px;
+    z-index: 4;
+  }
+
+  .rc-slider-step {
+    background-color: ${({ theme, disabled }) => (
+    disabled ? theme.color.miscLightest : theme.color.miscLight
+  )};
+    height: 6px;
+    top: 3px;
+    border-radius: 3px;
+  }
+
+  .rc-slider-disabled {
+    background-color: inherit;
+  }
+`
+
+interface StyledHandlerProps extends HandleProps {
+  theme: {
+    color: TColor,
+  },
+  disabled: boolean,
+}
+
+function getHandlerColor(props: StyledHandlerProps): string {
+  const { min, max, value, theme, disabled } = props
+
+  if (disabled) return theme.color.miscDark
+
+  if ([min, max].includes(value)) return theme.color.miscDark
+
+  return theme.color.primaryDark
+}
+
+const StyledHandleWrapper = styled(Handle)`
+  z-index: 4;
+  box-shadow: none;
+`
+
+const StyledHandle = styled.div<StyledHandlerProps>`
+  position: absolute;
+  ${switchTransition}
+  background-color: ${getHandlerColor};
+  transition-property: background-color, box-shadow;
+  border-radius: 50%;
+  cursor: pointer;
+  height: 18px;
+  width: 18px;
+  top: -5px;
+  left: 5px;
+  z-index: 4;
+  transform: translateX(-50%);
+  border: none;
+
+  &:hover, &:active {
+    background-color: ${({ theme, disabled }) => !disabled && theme.color.primaryDark};
+    box-shadow: 0px 2px 4px rgba(98, 112, 139, 0.6);
+  }
+`
+
+export { StyledRangeWrapper, StyledHandleWrapper, StyledHandle }
