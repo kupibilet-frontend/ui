b5f1f2d7ac645b8c3abf942d68b35c0c08d99e2f
Ксения Губа <kseniaguba@Ksenias-MacBook-Pro.local>
2021-04-06 13:47:57 +0300

    util theme to ts

---
+++ src/utils/theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from 'styled-components'
+import { COLOR_NAMES } from 'components/ThemeProvider/types'
+
+export const getThemeColor = (
+  theme: DefaultTheme,
+  colorKey: COLOR_NAMES,
+  fallbackColor = 'transparent',
+): string => {
+  if (process.env.NODE_ENV === 'production' && colorKey && !(colorKey in theme.color)) {
+    /* eslint-disable no-console */
+    console.warn(`Unknown color "${colorKey}" supplied`)
+  }
+
+  return theme.color[colorKey] || fallbackColor
+}
