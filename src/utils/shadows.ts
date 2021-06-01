11fb845d842261e38fd5c21f2edea42e5fb9bf55
zinovev <an.a.zinovev@gmail.com>
2021-06-01 13:47:48 +0300

    add new shadows

--- src/utils/shadows.ts
+++ src/utils/shadows.ts
@@ -1,7 +1,5 @@
-export const shadowSmall = `
-  box-shadow: 0 3px 9px 0 rgba(98, 112, 139, .7);
-`
+export const shadow1 = 'box-shadow: 0px 1px 3px rgba(98, 112, 139, 0.2);'
 
-export const shadowLarge = `
-  box-shadow: 0 6px 18px 0 rgba(98, 112, 139, .7);
-`
+export const shadow2 = 'box-shadow: 0px 8px 15px rgba(98, 112, 139, 0.15);'
+
+export const shadow3 = 'box-shadow: 0px 13px 63px rgba(98, 112, 139, 0.2);'
