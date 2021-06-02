dfca542debd927858a2cede71ea1f9173be81fd3
MichaelShipitsyn <38543952+MichaelShipitsyn@users.noreply.github.com>
2021-06-02 15:30:47 +0300

    Fix checkbox disabled styles

--- src/components/Checkbox/helpers.ts
+++ src/components/Checkbox/helpers.ts
@@ -1,14 +1,15 @@
 import { DefaultTheme } from 'styled-components'
 
 function getBackgroundColor(disabled: boolean, checked: boolean, theme: DefaultTheme): string {
-  if (disabled) return theme.color.miscLightest
+  if (disabled && checked) return theme.color.primaryLighter
   if (checked) return theme.color.primaryDark
 
   return theme.color.background
 }
 
 function getShadowColor(disabled: boolean, checked: boolean, theme: DefaultTheme): string {
-  if (disabled) return theme.color.miscLight
+  if (disabled && !checked) return theme.color.miscLighter
+  if (disabled && checked) return theme.color.primaryLighter
   if (checked) return theme.color.primaryDark
 
   return theme.color.misc
