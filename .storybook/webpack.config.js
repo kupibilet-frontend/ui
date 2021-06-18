d9c0b8170cb5e16f6b91252ee8572e5d453351b1
zinovev <an.a.zinovev@gmail.com>
2021-06-18 16:08:22 +0300

    remove storybook addons

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,13 +5,6 @@ module.exports = function({ config }) {
   config.resolve.extensions.push('.tsx')
 
   config.module.rules.push({
-    test: /\.js$/,
-    include: [path.resolve(__dirname, '../src')],
-    loader: require.resolve('@storybook/source-loader'),
-    enforce: 'pre',
-  })
-
-  config.module.rules.push({
     include: [path.resolve(__dirname, '../src')],
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
