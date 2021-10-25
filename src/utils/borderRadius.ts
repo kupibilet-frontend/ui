3bb0058d250d30513257473f07a67e183d8933ce
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-10-25 14:10:48 +0300

    use new border radiuses

--- src/utils/borderRadius.ts
+++ src/utils/borderRadius.ts
@@ -9,20 +9,20 @@
  */
 
 export const borderRadiusSmall = {
-  left: 'border-radius: 4px 0 0 4px;',
-  right: 'border-radius: 0 4px 4px 0;',
-  top: 'border-radius: 4px 4px 0 0;',
-  bottom: 'border-radius: 0 0 4px 4px;',
-  all: 'border-radius: 4px;',
-  toString: (): string => borderRadiusSmall.all,
-}
-
-export const borderRadiusLarge = {
   left: 'border-radius: 8px 0 0 8px;',
   right: 'border-radius: 0 8px 8px 0;',
   top: 'border-radius: 8px 8px 0 0;',
   bottom: 'border-radius: 0 0 8px 8px;',
   all: 'border-radius: 8px;',
+  toString: (): string => borderRadiusSmall.all,
+}
+
+export const borderRadiusLarge = {
+  left: 'border-radius: 12px 0 0 12px;',
+  right: 'border-radius: 0 12px 12px 0;',
+  top: 'border-radius: 12px 12px 0 0;',
+  bottom: 'border-radius: 0 0 12px 12px;',
+  all: 'border-radius: 12px;',
   toString: (): string => borderRadiusLarge.all,
 }
 
