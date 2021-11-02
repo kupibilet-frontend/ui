956ed00f4c17b1ada4c192ff48777da9fe6e88da
Vladimir Nikitin <vladimirsuperwizard@gmail.com>
2021-11-02 15:21:55 +0300

    change header font-size and line-height as for new redesign

--- src/components/Typography/H1.ts
+++ src/components/Typography/H1.ts
@@ -2,15 +2,20 @@ import styled from 'styled-components'
 import { queries } from 'utils/media-queries'
 
 const H1 = styled.h1`
-  font-size: 44px;
-  line-height: 44px;
+  font-size: 36;
+  line-height: 48px;
   margin: 0;
   font-weight: 600;
   color: ${({ theme }) => theme.color.colorTextPrimary};
 
-  @media ${queries.isHandheld} {
+  @media ${queries.isTablet} {
+    font-size: 32px;
+    line-height: 40px;
+  }
+  
+  @media ${queries.isMobile} {
     font-size: 24px;
-    line-height: 28px;
+    line-height: 32px;
   }
 `
 
--- src/components/Typography/H2.ts
+++ src/components/Typography/H2.ts
@@ -2,15 +2,20 @@ import styled from 'styled-components'
 import { queries } from 'utils/media-queries'
 
 const H2 = styled.h2`
-  font-size: 38px;
-  line-height: 38px;
+  font-size: 28px;
+  line-height: 40px;
   margin: 0;
   font-weight: 600;
   color: ${({ theme }) => theme.color.colorTextPrimary};
 
-  @media ${queries.isHandheld} {
+  @media ${queries.isTablet} {
     font-size: 24px;
-    line-height: 28px;
+    line-height: 36px;
+  }
+  
+  @media ${queries.isMobile} {
+    font-size: 20px;
+    line-height: 26px;
   }
 `
 
