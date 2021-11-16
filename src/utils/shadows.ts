5934d4a743a6699981c14c8d8ad2c3e271f1cfba
zinovev <anatolii.zinovev@kupibilet.ru>
2021-11-16 15:54:02 +0300

    update box shadows

--- src/utils/shadows.ts
+++ src/utils/shadows.ts
@@ -1,5 +1,8 @@
-export const shadow1 = 'box-shadow: 0px 1px 3px rgba(98, 112, 139, 0.2);'
 
-export const shadow2 = 'box-shadow: 0px 8px 15px rgba(98, 112, 139, 0.15);'
+export const shadowFlat = 'box-shadow: 0px 1px 4px rgba(33, 33, 38, 0.1);'
 
-export const shadow3 = 'box-shadow: 0px 13px 63px rgba(98, 112, 139, 0.2);'
+export const shadowActive = 'box-shadow: 0px 8px 16px rgba(33, 33, 38, 0.08);'
+
+export const shadowElevate = 'box-shadow: 0px 8px 40px rgba(33, 33, 38, 0.1);'
+
+export const shadowPopup = 'box-shadow: 0px 24px 48px rgba(33, 33, 38, 0.4);'
