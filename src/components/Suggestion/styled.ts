a6f6a12220a5ab56a2d46fe4cd5edce4ab25f58b
zinovev <anatolii.zinovev@kupibilet.ru>
2021-11-17 15:43:13 +0300

    update select item colors

--- src/components/Suggestion/styled.ts
+++ src/components/Suggestion/styled.ts
@@ -16,17 +16,17 @@ export const SuggestionContainer = styled.div<TSuggestionContainerProps>`
   cursor: pointer;
   color: ${({ theme }) => theme.color.text600};
   background-color: ${({ theme, isHighlighted }) => (isHighlighted
-    ? theme.color.secondary100
+    ? theme.color.colorBgPrimaryHover
     : theme.color.background
   )};
 
   &:hover {
-    background: ${({ theme }) => theme.color.secondary100};
+    background: ${({ theme }) => theme.color.colorBgPrimaryHover};
     .icon-inherit-color {
       fill: ${({ theme }) => theme.color.text600};
     }
     &::after {
-      background: linear-gradient(to left, ${({ theme }) => theme.color.secondary100} 25%, transparent 0%);
+      background: linear-gradient(to left, ${({ theme }) => theme.color.colorBgPrimaryHover} 25%, transparent 0%);
     }
   }
   &::after {
@@ -37,7 +37,7 @@ export const SuggestionContainer = styled.div<TSuggestionContainerProps>`
     width: 24px;
     background: linear-gradient(to left,
       ${({ theme, isHighlighted }) => (isHighlighted
-    ? theme.color.secondary100
+    ? theme.color.colorBgPrimaryHover
     : theme.color.background
   )} 25%, transparent 0%);
   }
