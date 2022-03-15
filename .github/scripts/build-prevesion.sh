99db930183e1e5cb6900ef751e8c32171980eaee
privetkometa <privetkometa@gmail.com>
2022-03-15 15:33:43 +0300

    6
    Signed-off-by: privetkometa <privetkometa@gmail.com>

---
+++ .github/scripts/build-prevesion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+yarn versionize || exit $?
+
+VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
+COMMENT="New version of package: \`$VERSION\`"
+
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+# Exit with npm exit-code when it fails
+npm publish --tag dev || exit $?
+curl \
+  -sS \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -X POST \
