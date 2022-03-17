42fde560a141fb8236a8c86998a8fc882fe157dd
Ksenia <ksenia.guba@kupibilet.ru>
2022-03-17 13:43:02 +0300

    16

--- .github/scripts/build-prevesion.sh
+++ .github/scripts/build-prevesion.sh
@@ -8,7 +8,7 @@ yarn versionize || exit $?
 VERSION=$(node -e "process.stdout.write(require('./package.json').version)")
 COMMENT="New version of package: \`$VERSION\`"
 
-echo "https://registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 # Exit with npm exit-code when it fails
 npm publish --tag dev || exit $?
 curl \
