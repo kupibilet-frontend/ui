f98c5b0550ce38ed1159733f887040da574af126
Ksenia <ksenia.guba@kupibilet.ru>
2022-03-17 13:52:38 +0300

    17

--- .github/scripts/build-prevesion.sh
+++ .github/scripts/build-prevesion.sh
@@ -11,7 +11,4 @@ COMMENT="New version of package: \`$VERSION\`"
 echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > .npmrc
 # Exit with npm exit-code when it fails
 npm publish --tag dev || exit $?
-curl \
-  -sS \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -X POST \
+
