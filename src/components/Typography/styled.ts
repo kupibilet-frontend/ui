51a0583dcc87baf77a6459f28ad9e0b3d950989b
Alexander Sapozhnikov <sapozhnikov@kupibilet.ru>
2022-03-18 18:25:56 +0300

    replace WithMedia2021 to queries2021 in Typography

--- src/components/Typography/styled.ts
+++ src/components/Typography/styled.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components'
 import { COLOR_NAMES } from 'components/ThemeProvider/types'
+import { queries2021 } from 'utils/media-queries'
 import { TTokenName } from 'components/Typography'
 
 interface TStyledTypography {
     color?: COLOR_NAMES,
     tokenName: TTokenName,
+    mobileTokenName: TTokenName,
     className?: string;
 }
 
@@ -16,4 +18,10 @@ export const StyledTypography = styled.span<TStyledTypography>`
   text-decoration: none;
   text-decoration-skip-ink: none;
   margin: 0;
+
+  @media ${queries2021.isMobile} {
+    font-size: ${({ theme, mobileTokenName }) => theme.typography[mobileTokenName].size}px;
+    font-weight: ${({ theme, mobileTokenName }) => theme.typography[mobileTokenName].fontWeight};
+    line-height: ${({ theme, mobileTokenName }) => theme.typography[mobileTokenName].lineHeight}px;
+  }
 `
