768e829850e47b7d1db4229190bb08d24a5c1ec3
Alexander Sapozhnikov <sapozhnikov@kupibilet.ru>
2022-05-27 16:19:24 +0300

    refresh tokens

--- src/components/ThemeProvider/tokens/light/tag.ts
+++ src/components/ThemeProvider/tokens/light/tag.ts
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Tue, 05 Apr 2022 15:09:57 GMT
+ * Generated on Fri, 27 May 2022 13:17:33 GMT
  */
 
 export const tag_label_normal_medium_color_bg_default = "#eeeff2";
@@ -10,7 +10,7 @@ export const tag_label_normal_medium_typography_desktop_default = {"fontFamily":
 export const tag_label_normal_medium_size_border_radius_default = "4px";
 export const tag_label_normal_medium_size_padding_default = {"top":"4px","right":"8px","bottom":"4px","left":"8px","innerHorizontal":"4px"};
 export const tag_label_warning_medium_color_bg_default = "#fff6eb";
-export const tag_label_warning_medium_color_text_default = "#ff8b00";
+export const tag_label_warning_medium_color_text_default = "#ff9000";
 export const tag_label_warning_medium_typography_mobile_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_warning_medium_typography_desktop_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_warning_medium_size_border_radius_default = "4px";
@@ -22,7 +22,7 @@ export const tag_label_success_medium_typography_desktop_default = {"fontFamily"
 export const tag_label_success_medium_size_border_radius_default = "4px";
 export const tag_label_success_medium_size_padding_default = {"top":"4px","right":"8px","bottom":"4px","left":"8px","innerHorizontal":"4px"};
 export const tag_label_danger_medium_color_bg_default = "#fcede8";
-export const tag_label_danger_medium_color_text_default = "#de350b";
+export const tag_label_danger_medium_color_text_default = "#e64926";
 export const tag_label_danger_medium_typography_mobile_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_danger_medium_typography_desktop_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_danger_medium_size_border_radius_default = "4px";
@@ -33,8 +33,8 @@ export const tag_label_info_medium_typography_mobile_default = {"fontFamily":"Su
 export const tag_label_info_medium_typography_desktop_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_info_medium_size_border_radius_default = "4px";
 export const tag_label_info_medium_size_padding_default = {"top":"4px","right":"8px","bottom":"4px","left":"8px","innerHorizontal":"4px"};
-export const tag_label_primary_medium_color_bg_default = "#ffefee";
-export const tag_label_primary_medium_color_text_default = "#fa6258";
+export const tag_label_primary_medium_color_bg_default = "#ebf3ff";
+export const tag_label_primary_medium_color_text_default = "#0f99f5";
 export const tag_label_primary_medium_typography_mobile_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_primary_medium_typography_desktop_default = {"fontFamily":"SuisseIntl-Medium","size":"12","lineHeight":"18","fontWeight":"500"};
 export const tag_label_primary_medium_size_border_radius_default = "4px";
---
+++ style-dictionary/tokens/README.md
@@ -0,0 +1,2 @@
+# design-tokens
+
