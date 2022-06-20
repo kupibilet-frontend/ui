b9a4f9b1927e36b9f67082218bf2ca1133b34eda
zinovev <anatolii.zinovev@kupibilet.ru>
2022-06-20 17:47:16 +0300

    add react responsive media impl

--- src/utils/media-queries.tsx
+++ src/utils/media-queries.tsx
@@ -4,11 +4,16 @@ import { TWithMediaProps } from 'utils/types'
 // @ts-ignore delete old package
 import Media from '@kupibilet/react-media'
 
+import { useMediaQuery } from 'react-responsive'
+
+export const MOBILE_BREAKPOINT = 599
+export const TABLET_BREAKPOINT = 1199
+
 export const queries = {
-  isMobile: 'screen and (max-width: 599px)',
-  isTablet: 'screen and (min-width: 600px) and (max-width: 1199px)',
-  isHandheld: 'screen and (max-width: 1199px)',
-  isDesktop: 'screen and (min-width: 1200px)',
+  isMobile: `screen and (max-width: ${MOBILE_BREAKPOINT}px)`,
+  isTablet: `screen and (min-width: ${MOBILE_BREAKPOINT + 1}px) and (max-width: ${TABLET_BREAKPOINT}px)`,
+  isHandheld: `screen and (max-width: ${TABLET_BREAKPOINT}px)`,
+  isDesktop: `screen and (min-width: ${TABLET_BREAKPOINT + 1}px)`,
 }
 
 /**
@@ -17,7 +22,7 @@ export const queries = {
  * export default withMedia(Component)
  */
 
-export function withMedia<T extends TWithMediaProps>(
+export function withMediaOld<T extends TWithMediaProps>(
   Component:React.ComponentType<T>,
 ): (props: Partial<T>) => JSX.Element {
   function MediaProvider(props: Partial<T>): JSX.Element {
@@ -35,6 +40,28 @@ export function withMedia<T extends TWithMediaProps>(
   MediaProvider.displayName = 'withMedia'
 
   return MediaProvider
+}
+
+
+export function withMedia<T>(Component: React.ComponentType<T>): any {
+  function WrpappedComponnet(props: T) {
+    const isMobile = useMediaQuery({ query: queries.isMobile })
+    const isTablet = useMediaQuery({ query: queries.isTablet })
+    const isHandheld = useMediaQuery({ query: queries.isHandheld })
+    const isDesktop = useMediaQuery({ query: queries.isDesktop })
+
+    return (
+      <Component
+        {...props}
+        isMobile={isMobile}
+        isTablet={isTablet}
+        isHandheld={isHandheld}
+        isDesktop={isDesktop}
+      />
+    )
+  }
+
+  return WrpappedComponnet
 }
 
 const media = {
