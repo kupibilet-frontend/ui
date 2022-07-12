3e6d6a30555f1dba54fb21db25f235aee8bac1cc
zinovev <anatolii.zinovev@kupibilet.ru>
2022-07-12 11:16:57 +0300

    improve Button types

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export type TButtonSize = 'small' | 'medium' | 'large'
 
 export type TNeighboringInGroupType = 'both' | 'left' | 'right' | null
@@ -9,7 +11,7 @@ export type TButtonVariant = 'primary' | 'secondary' | 'contrast' | 'link' | 'he
 export interface TButtonProps {
   size?: TButtonSize,
   variant?: TButtonVariant,
-  children?: React.ReactNode,
+  children?: React.ReactNode | React.ReactNode[],
   disabled?: boolean,
   icon?: React.ReactNode,
   leftIcon?: React.ReactNode,
@@ -18,7 +20,7 @@ export interface TButtonProps {
   neighboringInGroup?: TNeighboringInGroupType,
   href?: string,
   target?: string,
-  onClick?: (event: Event) => void,
+  onClick?: (e: React.MouseEvent<HTMLButtonElement> | React.MouseEvent<HTMLAnchorElement>) => void,
   type?: string,
   tabIndex?: number,
   withTextUnderline?: boolean,
