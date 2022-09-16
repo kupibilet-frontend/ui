bcf1f26385fac65d2fd7c6d48588796f29f411a0
zinovev <anatolii.zinovev@kupibilet.ru>
2022-09-16 14:30:16 +0300

    update tokens

--- src/components/ThemeProvider/tokens/switcher.ts
+++ src/components/ThemeProvider/tokens/switcher.ts
@@ -1,15 +1,15 @@
 /**
  * Do not edit directly
- * Generated on Wed, 14 Sep 2022 12:24:15 GMT
+ * Generated on Fri, 16 Sep 2022 11:27:29 GMT
  */
 
 export const switcher_default_medium_color_bg_normal = "#d0d1d7";
 export const switcher_default_medium_color_bg_hover = "#d0d1d7";
-export const switcher_default_medium_color_bg_active = "#37B866";
+export const switcher_default_medium_color_bg_active = "#007acc";
 export const switcher_default_medium_color_bg_disable = "#eeeff2";
-export const switcher_default_medium_color_border_normal = "rgba(0, 0, 0, 0)";
-export const switcher_default_medium_color_border_hover = "#37B866";
-export const switcher_default_medium_color_border_active = "#37B866";
+export const switcher_default_medium_color_border_normal = "#d0d1d7";
+export const switcher_default_medium_color_border_hover = "#008ae6";
+export const switcher_default_medium_color_border_active = "#007acc";
 export const switcher_default_medium_color_border_disable = "#eeeff2";
 export const switcher_default_medium_color_switcher_icon_normal = "#ffffff";
 export const switcher_default_medium_color_switcher_icon_hover = "#ffffff";
