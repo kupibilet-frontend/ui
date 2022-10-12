af7024ef7a5c57357c5792a8610ec4c76bc929f2
zhenya <evgenii.bugaev@kupibilet.ru>
2022-10-12 15:11:45 +0400

    icon stroke color

--- src/components/Icon/styled.ts
+++ src/components/Icon/styled.ts
@@ -32,6 +32,7 @@ interface TIconSvgProps {
   size: ICON_SIZES,
   striked: boolean,
   fill: COLOR_NAMES,
+  stroke: COLOR_NAMES,
 }
 
 const IconSvg = styled.svg<TIconSvgProps>`
@@ -40,6 +41,7 @@ const IconSvg = styled.svg<TIconSvgProps>`
   height: ${({ size }) => sizes[size]}px;
   width: ${({ size }) => sizes[size]}px;
   fill: ${({ theme, fill }) => getThemeColor(theme, fill)};
+  stroke: ${({ theme, stroke }) => getThemeColor(theme, stroke)};
   opacity: ${({ striked }) => (striked ? 0.6 : 1)};
 
   ${({ rotate }) => {
