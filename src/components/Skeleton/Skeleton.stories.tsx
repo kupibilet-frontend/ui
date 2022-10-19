4e529898387c3b8699f97a43fa7a35a393aed105
zhenya <evgenii.bugaev@kupibilet.ru>
2022-10-19 17:42:51 +0400

    added skeleton

---
+++ src/components/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { Skeleton } from 'components/Skeleton'
+
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+
+export default {
+  title: 'Skeleton',
+  component: Skeleton,
+} as ComponentMeta<typeof Skeleton>
+
+const Template: ComponentStory<typeof Skeleton> = (args) => (
+  <Skeleton {...args}>
+    {args.children}
+  </Skeleton>
+)
+
+export const SkeletonBase = Template.bind({})
+SkeletonBase.args = {
+  width: 200,
+  height: 50,
+}
+
+export const SkeletonCircular = Template.bind({})
+SkeletonCircular.args = {
+  width: 50,
+  height: 50,
+  variant: 'circular',
+}
+
+export const SkeletonWithChildren = Template.bind({})
+SkeletonWithChildren.args = {
+  children: <div style={{ width: '50px', height: '50px' }} />,
+}
---
+++ src/components/Skeleton/index.tsx
@@ -0,0 +1,83 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+
+
+type StylesProps = {hasChildren: boolean} & SkeletonProps
+
+const choiceOfWidth = ({ hasChildren, width }:StylesProps) => {
+  if (width) return `${width}px`
+
+  if (hasChildren && !width) {
+    return 'fit-content'
+  }
+
+  return ''
+}
+
+const choiceOfHeight = ({ hasChildren, height }:StylesProps) => {
+  if (height) return `${height}px`
+
+  if (hasChildren && !height) {
+    return 'auto'
+  }
+
+  return ''
+}
+
+const skeletonAnimation = keyframes`
+  0% {
+    left: -100%;
+  }
+  50% {
+    left: 50%;
+  }
+  100% {
+    left: 200%;
+  }
+`
+
+
+const StyledSkeleton = styled.div<StylesProps>`
+  position: relative;
+  overflow: hidden;
+  border-radius: ${({ variant }) => (variant === 'circular' ? '50%' : '4px')};
+  width: ${choiceOfWidth};
+  height: ${choiceOfHeight};
+  background: ${({ theme }) => theme.color.colorBgSecondaryNormal};
+  &::after {
+    content: '';
+    animation: ${skeletonAnimation} 1.6s linear .1s infinite;
+    background: ${({ theme }) => theme.color.colorBgPrimaryNormal};
+    filter: blur(8.5px);
+    position: absolute;
+    transform: translateX(-100%); 
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    width: 15%;
+}
+`
+
+
+type SkeletonProps = {
+  width?: number;
+  height?: number;
+  variant: 'circular' | 'rounded'
+}
+
+// eslint-disable-next-line react/prop-types
+export const Skeleton: React.FC<SkeletonProps> = ({ width, height, children, variant }) => {
+  const hasChildren = Boolean(children)
+
+  return (
+    <StyledSkeleton
+      width={width}
+      height={height}
+      hasChildren={hasChildren}
+      variant={variant}
+    >
+      {children}
+    </StyledSkeleton>
+  )
+}
