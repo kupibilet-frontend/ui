62ec1631f2b316d472b77c15983f6d11963e44f5
zhenya <evgenii.bugaev@kupibilet.ru>
2022-11-28 18:26:25 +0800

    new animation

--- src/components/Skeleton/index.tsx
+++ src/components/Skeleton/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import styled, { DefaultTheme, keyframes } from 'styled-components'
 
 
 type StylesProps = {hasChildren: boolean} & SkeletonProps
@@ -25,17 +25,12 @@ const choiceOfHeight = ({ hasChildren, height }:StylesProps) => {
 }
 
 const skeletonAnimation = keyframes`
-  0% {
-    left: -50%;
-  }
-  50% {
-    left: 50%;
-  }
-  100% {
-    left: 200%;
-  }
+  0%{background-position: 100vw 0}
+  to{background-position: 250vw 0}
 `
 
+// eslint-disable-next-line max-len
+const skeletonLineColor = ({ theme }: { theme: DefaultTheme}) => theme.skeleton.skeleton_default_default_color_bg_default
 
 const StyledSkeleton = styled.div<StylesProps>`
   position: relative;
@@ -44,23 +39,25 @@ const StyledSkeleton = styled.div<StylesProps>`
   width: ${choiceOfWidth};
   height: ${choiceOfHeight};
   background: ${({ theme }) => theme.skeleton.skeleton_default_default_color_bg_default};
-  &::after {
-    content: '';
-    animation: ${skeletonAnimation} 1.6s linear 1s infinite;
-    background: ${({ theme }) => theme.skeleton.skeleton_default_default_color_primary_default};
-    filter: blur(8.5px);
-    position: absolute;
-    transform: translateX(-100%); 
-    bottom: 0;
-    left: 0;
-    right: 0;
-    top: 0;
-    width: 15%;
-    max-width: 64px;
-    min-width: 24px;
-}
+  background: 
+    linear-gradient(270deg, 
+      ${skeletonLineColor} 0%, 
+      ${skeletonLineColor} 42.4%, 
+      rgba(237, 238, 243, 0) 54.38%,
+      ${skeletonLineColor} 66.88%, ${skeletonLineColor} 100%),
+      linear-gradient(270deg, 
+        ${skeletonLineColor} 0%,
+        ${skeletonLineColor} 39.27%, 
+        rgba(237, 238, 243, 0) 54.9%, 
+        ${skeletonLineColor} 100%);
+  background-size: 150vw 104px;
+  position: relative;
+  animation-name: ${skeletonAnimation};
+  animation-duration: 1.6s;
+  animation-fill-mode: forwards;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear; 
 `
-
 
 type SkeletonProps = {
   width?: number;
